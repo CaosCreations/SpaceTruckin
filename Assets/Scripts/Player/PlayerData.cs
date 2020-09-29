15229808b3c33adde39a7e0ea671d73df216e266
pseudowalrus <61809127+pseudowalrus@users.noreply.github.com>
2020-09-29 18:57:46 +0100

    Prefabs and UI initialisation logic
    Also made playerMoney static so it could be accessed easily by the vendingmachineitem prefab. Probably should have been static anyway.

--- Assets/Scripts/Player/PlayerData.cs
+++ Assets/Scripts/Player/PlayerData.cs
@@ -6,5 +6,5 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "PlayerData", menuName = "ScriptableObjects/PlayerData", order = 1)]
 public class PlayerData : ScriptableObject
 {
-    public long playerMoney;
+    public static long playerMoney;
 }
---
+++ Assets/Scripts/UI/VendingMachine/VendingMachineManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VendingMachineManager : MonoBehaviour
+{
+    // Items indices correspond to the keys on the keypad 
+    public VendingMachineItem[] items;
+
+    public GameObject[] itemObjects;
+
+    public GameObject itemContainer;
+    public GameObject keypadContainer; 
+
+    public GameObject itemPrefab; 
+    public GameObject keyPrefab;
+
+    public Text digitText;
+    public Text feedbackText; 
+
+    private float numberOfKeys = 10; 
+    private Color feedbackColour = Color.green;
+
+    private void Start()
+    {
+        InitialiseItems();
+        InitialiseKeypad(); 
+    }
+
+
+    private void InitialiseItems()
+    {
+        for (int i = 0; i < items.Length; i++)
+        {
+            GameObject item = Instantiate(itemPrefab, itemContainer.transform);
+            itemObjects[i] = item; 
+            item.name = "Item " + i.ToString();
+
+            Text text = item.GetComponent<Text>();
+            text.text = items[i].itemName;
+
+            Image image = GetComponent<Image>();
+            image.sprite = items[i].sprite; 
+        }
+    }
+
+    private void InitialiseKeypad()
+    {
+        for (int i = 0; i < numberOfKeys; i++)
+        {
+            GameObject key = Instantiate(keyPrefab, keypadContainer.transform);
+            key.name = "Key " + i.ToString();
+
+            Text text = key.GetComponentInChildren<Text>();
+            text.text = i.ToString();
+
+            Button button = key.GetComponent<Button>();
+            button.onClick.RemoveAllListeners();
+            button.onClick.AddListener(delegate
+            {
+                items[i].PurchaseItem();
+                itemObjects[i].GetComponent<Image>().color = feedbackColour;
+            });
+        }
+    }
+}
