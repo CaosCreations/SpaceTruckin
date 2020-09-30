6aa54c24e389bb71078f6321d087d3477eebf77f
pseudowalrus <61809127+pseudowalrus@users.noreply.github.com>
2020-09-30 09:37:58 +0100

    Addressing changes and fixing scrollview

--- Assets/Scripts/Player/PlayerData.cs
+++ Assets/Scripts/Player/PlayerData.cs
@@ -6,5 +6,11 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "PlayerData", menuName = "ScriptableObjects/PlayerData", order = 1)]
 public class PlayerData : ScriptableObject
 {
+    public static int playerStartingMoney; 
     public static long playerMoney;
+
+    public static void Init()
+    {
+        playerMoney = playerStartingMoney;
+    }
 }
