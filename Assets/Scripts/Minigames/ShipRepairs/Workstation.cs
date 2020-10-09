758fa41421df8c500c757b931bf1b77ec2f299d3
pseudowalrus <61809127+pseudowalrus@users.noreply.github.com>
2020-10-09 14:08:45 +0100

    Rotating workstation on z axis

--- Assets/Scripts/Minigames/ShipRepairs/Workstation.cs
+++ Assets/Scripts/Minigames/ShipRepairs/Workstation.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class Workstation : MonoBehaviour
 {
     public GameObject redZone;
-    public GameObject greenZone; 
+    public GameObject greenZone;
+    public GameObject indicator; 
 
     public Vector3 workstationRotation;
-        
-    public float rotationSpeed = 0.5f; 
+
+    public float rotationSpeed = 16f;
+    public float rotationAngle = 8f; 
 
     private void Start()
     {
@@ -18,7 +20,25 @@ public class Workstation : MonoBehaviour
 
     public void RotateWorkStation()
     {
-        transform.localEulerAngles += new Vector3(0f, 0f, rotationSpeed);
+        //transform.localEulerAngles += new Vector3(0f, 0f, rotationSpeed);
+        //transform.Rotate(Vector3.forward, rotationSpeed);
+        transform.Rotate(0f, 0f, rotationSpeed);
+    }
+
+    public void ReverseRotationDirection()
+    {
+        rotationSpeed = rotationSpeed > 0 ? rotationSpeed * -1 : Mathf.Abs(rotationSpeed); 
+    }
+
+    public void IncreaseRotationSpeed(float speedIncrease) 
+    {
+        rotationSpeed += speedIncrease;
+    }
+
+    //ncap
+    public void DecreaseGreenZoneSize(float sizeDecrease)
+    {
+        greenZone.transform.localScale -= new Vector3(sizeDecrease, 0f, 0f);
     }
 
     private void Update()
