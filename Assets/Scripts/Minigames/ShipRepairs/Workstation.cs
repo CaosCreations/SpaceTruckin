13689227a6e4008ef4864b5a25a717eab65778d9
pseudowalrus <61809127+pseudowalrus@users.noreply.github.com>
2020-10-09 11:08:56 +0100

    Workstation with red and green zones set up.

---
+++ Assets/Scripts/Minigames/ShipRepairs/Workstation.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Workstation : MonoBehaviour
+{
+    public GameObject redZone;
+    public GameObject greenZone; 
+
+    public Vector3 workstationRotation;
+        
+    public float rotationSpeed = 0.5f; 
+
+    private void Start()
+    {
+        workstationRotation = transform.localEulerAngles;
+    }
+
+    public void RotateWorkStation()
+    {
+        transform.localEulerAngles += new Vector3(0f, 0f, rotationSpeed);
+    }
+
+    private void Update()
+    {
+        RotateWorkStation(); 
+    }
+}
