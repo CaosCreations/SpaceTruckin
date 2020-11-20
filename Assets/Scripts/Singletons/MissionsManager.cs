82ee2bc70e93b11f5dba88bc2c66827450972055
Sabien <ambid17@gmail.com>
2020-11-20 17:58:46 -0600

    rename/move files

--- Assets/Scripts/Singletons/MissionsManager.cs
+++ Assets/Scripts/Singletons/MissionsManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class GameManager : MonoBehaviour
+public class MissionsManager : MonoBehaviour
 {
     public MissionContainer missionContainer;
 
