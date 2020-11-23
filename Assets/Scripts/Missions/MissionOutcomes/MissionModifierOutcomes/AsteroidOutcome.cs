8b67290e4e9dfa5a93960816f256af6da9347cf9
pseudowalrus <61809127+pseudowalrus@users.noreply.github.com>
2020-11-23 20:51:27 +0000

    Corrected asset menu path

--- Assets/Scripts/Missions/MissionOutcomes/MissionModifierOutcomes/AsteroidOutcome.cs
+++ Assets/Scripts/Missions/MissionOutcomes/MissionModifierOutcomes/AsteroidOutcome.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "AsteroidOutcome", menuName = "ScriptableObjects/Missions/Outcomes/AsteroidOutcome", order = 1)]
+[CreateAssetMenu(fileName = "AsteroidOutcome", menuName = "ScriptableObjects/Missions/Outcomes/MissionModifierOutcomes/AsteroidOutcome", order = 1)]
 public class AsteroidOutcome : MissionModifierOutcome
 {
 
