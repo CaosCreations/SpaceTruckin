829a55d0d2fe86552e942ce0c026147a829ec875
pseudowalrus <61809127+pseudowalrus@users.noreply.github.com>
2020-11-26 21:54:06 +0000

    Ships singleton

---
+++ Assets/Scripts/Singletons/ShipsManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class ShipsManager : MonoBehaviour
+{
+    public static ShipsManager Instance;
+
+    public Ship[] ships;
+
+    private void Awake()
+    {
+        if (FindObjectsOfType(GetType()).Length > 1)
+        {
+            Destroy(gameObject);
+        }
+
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else if (Instance == this)
+        {
+            Destroy(Instance.gameObject);
+            Instance = this;
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    public static void DamageShip(int shipId, int damage)
+    {
+        ships[shipId].hullIntegrity = Mathf.Max(0, ships[shipId].hullIntegrity - damage);
+    }
+}
