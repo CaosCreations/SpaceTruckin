71b8001f0bf08f5680f11dadd7580516b0b0565e
pseudowalrus <61809127+pseudowalrus@users.noreply.github.com>
2020-11-28 23:36:41 +0000

    Resolve omen xp buffs in the xp process method

--- Assets/Scripts/Missions/MissionOutcomes/OmenOutcome.cs
+++ Assets/Scripts/Missions/MissionOutcomes/OmenOutcome.cs
@@ -1,11 +1,12 @@
 ﻿using UnityEngine;
 
 /// <summary>
-/// Omens grant an xp buff or debuff determined by RNG
+    /// Omens grant an xp buff or debuff. 
+    /// The one that's granted is determined by the dice roll.
 /// </summary>
 
 [CreateAssetMenu(fileName = "OmenOutcome", menuName = "ScriptableObjects/Missions/Outcomes/OmenOutcome", order = 1)]
-public class OmenOutcome : ScriptableObject
+public class OmenOutcome : MissionOutcome
 {
     public float coefficient; 
 }
