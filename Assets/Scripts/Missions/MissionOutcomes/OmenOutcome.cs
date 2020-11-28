e2ecfd6c025b9f7db4eff4cbebf42a5f7f83dba2
pseudowalrus <61809127+pseudowalrus@users.noreply.github.com>
2020-11-28 23:16:40 +0000

    Omen model

---
+++ Assets/Scripts/Missions/MissionOutcomes/OmenOutcome.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Omens grant an xp buff or debuff determined by RNG
+/// </summary>
+
+[CreateAssetMenu(fileName = "OmenOutcome", menuName = "ScriptableObjects/Missions/Outcomes/OmenOutcome", order = 1)]
+public class OmenOutcome : ScriptableObject
+{
+    public float coefficient; 
+}
