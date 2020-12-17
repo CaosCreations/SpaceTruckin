56531cda88baf0d66d24a2415af4d88f1bc0c9d5
Chris Richmond <richmond.cd@gmail.com>
2020-12-17 13:41:45 -0600

    #21 Simple office door open/close mechanism

---
+++ Assets/OfficeDoor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//Simple handler for office door box collider
+public class OfficeDoor : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        transform.parent.Translate(100, 0, 0);
+        transform.Translate(-100, 0, 0);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        transform.parent.Translate(-100, 0, 0);
+        transform.Translate(100, 0, 0);
+    }
+}
