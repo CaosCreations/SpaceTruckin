728ece769ae38e219bb1ea6a41f67aa03c0d0f68
Chris Richmond <richmond.cd@gmail.com>
2020-12-17 15:49:31 -0600

    #21
    Added door movement offset vector variable to allow easier reuse

--- Assets/OfficeDoor.cs
+++ Assets/OfficeDoor.cs
@@ -3,18 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
+
 //Simple handler for office door box collider
 public class OfficeDoor : MonoBehaviour
 {
+
+    public Vector3 doorOpenOffset;
+
     private void OnTriggerEnter(Collider other)
     {
-        transform.parent.Translate(100, 0, 0);
-        transform.Translate(-100, 0, 0);
+        transform.parent.Translate(doorOpenOffset);
+        transform.Translate(-doorOpenOffset);
     }
 
     private void OnTriggerExit(Collider other)
     {
-        transform.parent.Translate(-100, 0, 0);
-        transform.Translate(100, 0, 0);
+        transform.parent.Translate(-doorOpenOffset);
+        transform.Translate(doorOpenOffset);
     }
 }
