05f080c2ad9e1fc97396b9929f11bd5994e54af5
Max Hooton <61809127+maxwellpark@users.noreply.github.com>
2020-12-21 17:56:44 +0000

    Editor window and label

---
+++ Assets/Editor/DeleteScriptableObjects.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class DeleteScriptableObjects: EditorWindow
+{
+    [MenuItem("Tools/CaosCreations/Delete Scriptable Objects")]
+    private static void DeleteData()
+    {
+        GetWindow<DeleteScriptableObjects>("Delete Scriptable Objects");
+    }
+
+    private void OnGUI()
+    {
+        GUILayout.Label("Select a scriptable object to delete:", EditorStyles.whiteLargeLabel);
+
+    }
+
+    // EditorUtils 
+    public static void ShowWindow()
+    {
+        // Pass in the script instead as a param 
+        EditorWindow.GetWindow<DeleteScriptableObjects>();
+    }
+
+}
