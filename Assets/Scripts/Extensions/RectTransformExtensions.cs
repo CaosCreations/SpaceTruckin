902ff4554f109030bea9caa250d0ec98cb6cefd1
pseudowalrus <61809127+pseudowalrus@users.noreply.github.com>
2020-12-23 19:49:04 +0000

    Extensions

---
+++ Assets/Scripts/Extensions/RectTransformExtensions.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public static class RectTransformExtensions
+{
+    public static void SetAnchors(this RectTransform self, System.ValueTuple<Vector2, Vector2> anchors)
+    {
+        self.anchorMin = anchors.Item1;
+        self.anchorMax = anchors.Item2;
+    }
+
+    public static void ResetRect(this RectTransform self)
+    {
+        // Set Top, Right, Bottom, Left to 0
+        self.offsetMin = Vector2.zero;
+        self.offsetMax = Vector2.zero;
+
+        // Set Scale to 1 
+        self.localScale = Vector2.one;
+    }
+
+}
---
+++ Assets/Scripts/Extensions/TextExtensions.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public static class TextExtensions
+{
+    public static void SetDefaultFont(this Text self)
+    {
+        self.font = Resources.GetBuiltinResource(typeof(Font), "Arial.ttf") as Font;
+        //return self;
+    }
+}
