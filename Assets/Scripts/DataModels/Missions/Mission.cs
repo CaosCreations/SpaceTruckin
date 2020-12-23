1a2a7936faacc83a4aeb6d323d13bcdb6cb8ba43
Max Hooton <61809127+maxwellpark@users.noreply.github.com>
2020-12-23 20:17:43 +0000

    MissionSaveData class

--- Assets/Scripts/DataModels/Missions/Mission.cs
+++ Assets/Scripts/DataModels/Missions/Mission.cs
@@ -1,8 +1,19 @@
-﻿using UnityEngine;
+﻿using System;
+using System.IO;
+using UnityEngine;
 
 [CreateAssetMenu(fileName = "Mission", menuName = "ScriptableObjects/Mission", order = 1)]
 public class Mission : ScriptableObject
 {
+    public class MissionSaveData
+    {
+        // Data to persist
+        [Header("Data to update IN GAME")]
+        public bool hasBeenAcceptedInNoticeBoard = false;
+        public Ship ship = null;
+        public int daysLeftToComplete;
+    }
+
     [Header("Set in Editor")]
     public int missionDurationInDays;
     public string missionName;
@@ -15,10 +26,7 @@ public class Mission : ScriptableObject
 
     // Data to persist
     [Header("Data to update IN GAME")]
-    public bool hasBeenAcceptedInNoticeBoard = false;
-    public Ship ship = null;
-    public int daysLeftToComplete;
-
+    public MissionSaveData missionSaveData;
 
     public void ProcessOutcomes()
     {
@@ -30,16 +38,66 @@ public class Mission : ScriptableObject
 
     public void ScheduleMission(Ship ship)
     {
-        this.ship = ship;
+        missionSaveData.ship = ship;
     }
 
     public void StartMission()
     {
-        daysLeftToComplete = missionDurationInDays;
+        missionSaveData.daysLeftToComplete = missionDurationInDays;
     }
     
     public bool IsInProgress()
     {
-        return daysLeftToComplete > 0;
+        return missionSaveData.daysLeftToComplete > 0;
+    }
+
+    public void Save()
+    {
+        Debug.Log($"Saving level {missionName}");
+        string folderPath = Path.Combine(Application.persistentDataPath, missionName);
+
+        if (!Directory.Exists(folderPath))
+        {
+            try
+            {
+                Directory.CreateDirectory(folderPath);
+            }
+            catch (Exception e)
+            {
+                Debug.LogError($"{e.Message}\n{e.StackTrace}");
+            }
+        }
+
+        string filePath = Path.Combine(folderPath, $"{missionName}.save");
+        string fileContents = JsonUtility.ToJson(missionSaveData);
+        try
+        {
+            File.WriteAllText(filePath, fileContents);
+        }
+        catch (Exception e)
+        {
+            Debug.LogError($"{e.Message}\n{e.StackTrace}");
+        }
+        Debug.Log($"Finished Saving level {missionName}");
+    }
+
+    public void Load()
+    {
+        Debug.Log($"loading level {missionName}");
+        string folderPath = Path.Combine(Application.persistentDataPath, missionName);
+        string filePath = Path.Combine(folderPath, $"{missionName}.save");
+        if (File.Exists(filePath))
+        {
+            try
+            {
+                string fileContents = File.ReadAllText(filePath);
+                //missionSaveData = JsonUtility.FromJson<PersistentLevelDataModel>(fileContents);
+            }
+            catch (Exception e)
+            {
+                Debug.LogError($"{e.Message}\n{e.StackTrace}");
+            }
+        }
+        Debug.Log($"Finished loading mission {missionName}");
     }
 }
