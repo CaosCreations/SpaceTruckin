9bae0388979b82e8c52ae1fe4f046911ad6e461a
Max Hooton <61809127+maxwellpark@users.noreply.github.com>
2020-12-27 14:51:15 +0000

    Get world position vertices of the roam boundary

--- Assets/Scripts/NPC/AIDestination.cs
+++ Assets/Scripts/NPC/AIDestination.cs
@@ -1,17 +1,40 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class AIDestination : MonoBehaviour
 {
-    // The x and z values that will determine the next position of the destination
-    private int xPos;
-    private int zPos;
+    // The object that determines the vertices of the roaming boundary.
+    public GameObject boundingPlane;
+
+    // The bounding area within which the destination can be set.  
+    private List<Vector3> boundaryVertices; 
+
+    private void Start()
+    {
+        Vector3[] vertices = boundingPlane.GetComponent<MeshFilter>().sharedMesh.vertices;
+        boundaryVertices = GetBoundaryVertices(vertices);
+    }
+
+    private List<Vector3> GetBoundaryVertices(Vector3[] vertices)
+    {
+        List<Vector3> boundaryVertices = new List<Vector3>();
+
+        // Unity planes have an 11x11 grid of points.
+        // We need to get the world position via TransformPoint.
+        boundaryVertices.Add(transform.TransformPoint(vertices[0]));
+        boundaryVertices.Add(transform.TransformPoint(vertices[10]));
+        boundaryVertices.Add(transform.TransformPoint(vertices[110]));
+        boundaryVertices.Add(transform.TransformPoint(vertices[120]));
+        
+        return boundaryVertices;
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("NPC"))
         {
-            xPos = Random.Range(1, 200);
-            zPos = Random.Range(1, 200);
+            float xPos = Random.Range(1, 200);
+            float zPos = Random.Range(1, 200);
             transform.position = new Vector3(xPos, transform.position.y, zPos);
         }
     }
