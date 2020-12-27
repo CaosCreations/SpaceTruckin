20e5edb16985acb3382d0c626837ac05c0eebd27
Max Hooton <61809127+maxwellpark@users.noreply.github.com>
2020-12-27 14:21:18 +0000

    Relocate NPC destination periodically

---
+++ Assets/Scripts/NPC/AIDestination.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class AIDestination : MonoBehaviour
+{
+    // The x and z values that will determine the next position of the destination
+    private int xPos;
+    private int zPos;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("NPC"))
+        {
+            xPos = Random.Range(1, 200);
+            zPos = Random.Range(1, 200);
+            transform.position = new Vector3(xPos, transform.position.y, zPos);
+        }
+    }
+}
