70a075882d44734e426ef7a1ea2c2d557f4aef73
Max Hooton <61809127+maxwellpark@users.noreply.github.com>
2020-12-27 12:44:01 +0000

    NPC prefab and skeleton script

---
+++ Assets/Scripts/NPC/NPCAgent.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class NPCAgent: MonoBehaviour
+{
+    public GameObject npcDestination;
+    private NavMeshAgent agent; 
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+
+    }
+
+    private void Update()
+    {
+        agent.SetDestination(npcDestination.transform.position);
+        
+    }
+}
