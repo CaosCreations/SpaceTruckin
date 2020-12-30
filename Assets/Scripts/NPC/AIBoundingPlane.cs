6a7d88fbbe13c26c5fb984855ed109f02e0c0e05
Max Hooton <61809127+maxwellpark@users.noreply.github.com>
2020-12-30 19:59:37 +0000

    Separating concerns

---
+++ Assets/Scripts/NPC/AIBoundingPlane.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class AIBoundingPlane : MonoBehaviour
+{
+    // The object that determines the vertices of the bounding area.
+
+    // The corners of the bounding area within which AI destinations can be set.  
+    public List<Vector3> boundaryVertices;
+
+    private void Awake()
+    {
+        Vector3[] vertices = GetComponent<MeshFilter>().sharedMesh.vertices;
+        boundaryVertices = GetBoundaryVertices(vertices);
+    }
+
+    /// <summary>
+    ///     Unity planes have an 11x11 grid of points.
+    ///     We need to get the world position via TransformPoint.
+    ///     The first two elements are the near corners, and the 
+    ///     last two are the far corners (depending on perspective).
+    /// </summary>
+    /// <param name="vertices"> The raw vertices of the plane's mesh. </param>
+    /// <returns> The four corners of the bounding plane. </returns>
+    private List<Vector3> GetBoundaryVertices(Vector3[] vertices)
+    {
+        List<Vector3> boundaryVertices = new List<Vector3>();
+        boundaryVertices.Add(transform.TransformPoint(vertices[0]));
+        boundaryVertices.Add(transform.TransformPoint(vertices[10]));
+        boundaryVertices.Add(transform.TransformPoint(vertices[110]));
+        boundaryVertices.Add(transform.TransformPoint(vertices[120]));
+
+        foreach (Vector3 v in boundaryVertices)
+        {
+            Debug.Log("Vert: " + v);
+        }
+
+        return boundaryVertices;
+    }
+
+    private void DrawBoundaryVertices()
+    {
+        Gizmos.color = Color.red;
+        if (boundaryVertices != null && boundaryVertices.Count > 0)
+        {
+            for (int i = 0; i < boundaryVertices.Count; i++)
+            {
+
+                Gizmos.DrawSphere(boundaryVertices[i], 0.25f);
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        DrawBoundaryVertices();
+    }
+}
--- Assets/Scripts/NPC/AIDestination.cs
+++ Assets/Scripts/NPC/AIDestination.cs
@@ -1,62 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class AIDestination : MonoBehaviour
 {
-    // The NPC that will move towards the attached game object.
-    // There is a one-to-one relationship between NPCs and destinations.
+    // The NPC that will move towards the destination.
     public NPCAgent npcAgent; 
 
-    // The object that determines the vertices of the roaming boundary.
-    public GameObject boundingPlane;
-
-    // The corners of the bounding area within which the destination can be set.  
-    private List<Vector3> boundaryVertices;
+    // This determines the area within which the destination can be set. 
+    private AIBoundingPlane aiBoundingPlane; 
 
     private BoxCollider boxCollider;
 
-    private void Awake()
+    private void Start()
     {
-        Vector3[] vertices = boundingPlane.GetComponent<MeshFilter>().sharedMesh.vertices;
-        boundaryVertices = GetBoundaryVertices(vertices);
+        aiBoundingPlane = GetComponentInParent<AIBoundingPlane>();
         boxCollider = GetComponent<BoxCollider>();
-
         MoveDestination();
-    }
-
-    /// <summary>
-    ///     Unity planes have an 11x11 grid of points.
-    ///     We need to get the world position via TransformPoint.
-    ///     The first two elements are the near corners, and the 
-    ///     last two are the far corners (depending on perspective).
-    /// </summary>
-    /// <param name="vertices"> The raw vertices of the plane's mesh. </param>
-    /// <returns> The four corners of the bounding plane. </returns>
-    private List<Vector3> GetBoundaryVertices(Vector3[] vertices)
-    {
-        List<Vector3> boundaryVertices = new List<Vector3>();
-        boundaryVertices.Add(boundingPlane.transform.TransformPoint(vertices[0]));
-        boundaryVertices.Add(boundingPlane.transform.TransformPoint(vertices[10]));
-        boundaryVertices.Add(boundingPlane.transform.TransformPoint(vertices[110]));
-        boundaryVertices.Add(boundingPlane.transform.TransformPoint(vertices[120]));
-
-        foreach (Vector3 v in boundaryVertices)
-        {
-            Debug.Log("Vert: " + v);
-        }
-        
-        return boundaryVertices;
     }
 
     private Tuple<float, float> GetNextDestination()
     {
         // The x and z values of the next destination's position.
         Tuple<float, float> xzPos = new Tuple<float, float>(
-            UnityEngine.Random.Range(boundaryVertices[0].x, boundaryVertices[1].x),
-            UnityEngine.Random.Range(boundaryVertices[0].z, boundaryVertices[2].z));
-
-        Debug.Log("New x: " + xzPos.Item1 + "\nNew z: " + xzPos.Item2);
+            UnityEngine.Random.Range(
+                aiBoundingPlane.boundaryVertices[0].x, aiBoundingPlane.boundaryVertices[1].x),
+            UnityEngine.Random.Range(
+                aiBoundingPlane.boundaryVertices[0].z, aiBoundingPlane.boundaryVertices[2].z));
 
         return xzPos;
     }
@@ -77,28 +46,17 @@ public class AIDestination : MonoBehaviour
         transform.position = new Vector3(xzPos.Item1, transform.position.y, xzPos.Item2);
     }
 
-    private void DrawBoundaryVertices()
-    {
-        Gizmos.color = Color.red;
-        if (boundaryVertices != null && boundaryVertices.Count > 0)
-        {
-            for (int i = 0; i < boundaryVertices.Count; i++)
-            {
-
-                Gizmos.DrawSphere(boundaryVertices[i], 0.25f);
-            }
-        }
-    }
-
     private void DrawDestination()
     {
         Gizmos.color = npcAgent.gizmoColour;
-        Gizmos.DrawWireCube(boxCollider.bounds.center, boxCollider.bounds.size);
+        if (boxCollider != null)
+        {
+            Gizmos.DrawWireCube(boxCollider.bounds.center, boxCollider.bounds.size);
+        }
     }
 
     private void OnDrawGizmos()
     {
-        DrawBoundaryVertices();
         DrawDestination();
     }
 }
