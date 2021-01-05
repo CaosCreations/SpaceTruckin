fc20a74d5d176486c3c1a801761352d5096d404c
Max Hooton <61809127+maxwellpark@users.noreply.github.com>
2021-01-05 23:18:28 +0000

    Get navigable buttons

---
+++ Assets/Scripts/UI/UIKeyboardNavigation.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIKeyboardNavigation : MonoBehaviour
+{
+    public TerminalUIManager terminalManager;
+    private ArrayList navigableButtons; 
+
+    private void Start()
+    {
+        navigableButtons = GetNavigableButtons();
+    }
+
+    private ArrayList GetNavigableButtons()
+    {
+        ArrayList navigableButtons = new ArrayList();
+
+        foreach (FieldInfo field in terminalManager.GetType().GetFields())
+        {
+            if (field.GetValue(terminalManager) is Button)
+            {
+                Debug.Log("Field is button");
+                navigableButtons.Add(field.GetValue(terminalManager));
+            }
+        }
+        return navigableButtons;
+    }
+
+}
