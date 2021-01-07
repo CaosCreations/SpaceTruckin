b79072be4e650f7429d8f844b72ece219e592bca
Max Hooton <61809127+maxwellpark@users.noreply.github.com>
2021-01-07 18:52:57 +0000

    Dynamic boundary vertex assignment

--- Assets/Scripts/NPC/AIBoundingPlane.cs
+++ Assets/Scripts/NPC/AIBoundingPlane.cs
@@ -3,10 +3,10 @@ using UnityEngine;
 
 public class AIBoundingPlane : MonoBehaviour
 {
-    // The object that determines the vertices of the bounding area.
-
     // The corners of the bounding area within which AI destinations can be set.  
     public List<Vector3> boundaryVertices;
+
+    private int[] vertexIndexes = new int[] { 0, 10, 110, 120 };
 
     private void Awake()
     {
@@ -16,25 +16,22 @@ public class AIBoundingPlane : MonoBehaviour
 
     /// <summary>
     ///     Unity planes have an 11x11 grid of points.
-    ///     We need to get the world position via TransformPoint.
+    ///     Index 0, 10, 110, and 120 are the four corners of the plane.
     ///     The first two elements are the near corners, and the 
     ///     last two are the far corners (depending on perspective).
+    ///     
+    ///     We need to get the world position via TransformPoint.
     /// </summary>
     /// <param name="vertices"> The raw vertices of the plane's mesh. </param>
     /// <returns> The four corners of the bounding plane. </returns>
     private List<Vector3> GetBoundaryVertices(Vector3[] vertices)
     {
         List<Vector3> boundaryVertices = new List<Vector3>();
-        boundaryVertices.Add(transform.TransformPoint(vertices[0]));
-        boundaryVertices.Add(transform.TransformPoint(vertices[10]));
-        boundaryVertices.Add(transform.TransformPoint(vertices[110]));
-        boundaryVertices.Add(transform.TransformPoint(vertices[120]));
 
-        foreach (Vector3 v in boundaryVertices)
+        foreach (int index in vertexIndexes)
         {
-            Debug.Log("Vert: " + v);
+            boundaryVertices.Add(transform.TransformPoint(vertices[index]));
         }
-
         return boundaryVertices;
     }
 
--- Assets/Scripts/NPC/AIDestination.cs
+++ Assets/Scripts/NPC/AIDestination.cs
@@ -6,7 +6,7 @@ public class AIDestination : MonoBehaviour
     // The NPC that will move towards the destination.
     public NPCAgent npcAgent; 
 
-    // This determines the area within which the destination can be set. 
+    // Determines the area within which the destination can be set. 
     private AIBoundingPlane aiBoundingPlane; 
 
     private BoxCollider boxCollider;
@@ -21,13 +21,13 @@ public class AIDestination : MonoBehaviour
     private Tuple<float, float> GetNextDestination()
     {
         // The x and z values of the next destination's position.
-        Tuple<float, float> xzPos = new Tuple<float, float>(
+        Tuple<float, float> xzPosition = new Tuple<float, float>(
             UnityEngine.Random.Range(
                 aiBoundingPlane.boundaryVertices[0].x, aiBoundingPlane.boundaryVertices[1].x),
             UnityEngine.Random.Range(
                 aiBoundingPlane.boundaryVertices[0].z, aiBoundingPlane.boundaryVertices[2].z));
 
-        return xzPos;
+        return xzPosition;
     }
 
     private void OnTriggerEnter(Collider other)
@@ -42,8 +42,8 @@ public class AIDestination : MonoBehaviour
 
     private void MoveDestination()
     {
-        Tuple<float, float> xzPos = GetNextDestination();
-        transform.position = new Vector3(xzPos.Item1, transform.position.y, xzPos.Item2);
+        Tuple<float, float> xzPosition = GetNextDestination();
+        transform.position = new Vector3(xzPosition.Item1, transform.position.y, xzPosition.Item2);
     }
 
     private void DrawDestination()
