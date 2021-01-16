9d70a45e0414b826672cd9a8feb4e1c2126e4938
Max Hooton <61809127+maxwellpark@users.noreply.github.com>
2021-01-16 14:54:54 +0000

    Ships init

--- Assets/Scripts/UI/Office/BedCanvasUI.cs
+++ Assets/Scripts/UI/Office/BedCanvasUI.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 public class BedCanvasUI : MonoBehaviour
@@ -56,5 +54,4 @@ public class BedCanvasUI : MonoBehaviour
         ShipsManager.Instance.SaveData();
         MessagesManager.Instance.SaveData();
     }
-
 }
