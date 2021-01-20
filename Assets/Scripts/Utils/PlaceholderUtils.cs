879d7b6b779fbff1c1673bb135828023597098b1
pseudowalrus <61809127+pseudowalrus@users.noreply.github.com>
2021-01-20 17:45:01 +0000

    Increase scroll view content height

--- Assets/Scripts/Utils/PlaceholderUtils.cs
+++ Assets/Scripts/Utils/PlaceholderUtils.cs
@@ -1,22 +1,14 @@
 ﻿public class PlaceholderUtils
 {
-    public static string LoremIpsumPassage = @"Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-    Quisque tortor dui, elementum eu convallis non, cursus ac dolor. 
-    Quisque dictum est quam, et pellentesque velit rutrum eget. 
-    Nullam interdum ultricies velit pharetra aliquet. Integer sodales a magna quis ornare. 
-    Ut vulputate nibh ipsum. Vivamus tincidunt nec nisi in fermentum. 
-    Mauris consequat mi vel odio consequat, eget gravida urna lobortis. 
-    Pellentesque eu ipsum consectetur, pharetra nulla in, consectetur turpis. 
-    Curabitur ornare eu nisi tempus varius. Phasellus vel ex mauris. 
-    Fusce fermentum mi id elementum gravida. ";
+    public static string LoremIpsumPassage = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque tortor dui, elementum eu convallis non, cursus ac dolor. Quisque dictum est quam, et pellentesque velit rutrum eget. Nullam interdum ultricies velit pharetra aliquet. Integer sodales a magna quis ornare. Ut vulputate nibh ipsum. Vivamus tincidunt nec nisi in fermentum. Mauris consequat mi vel odio consequat, eget gravida urna lobortis. Pellentesque eu ipsum consectetur, pharetra nulla in, consectetur turpis. Curabitur ornare eu nisi tempus varius. Phasellus vel ex mauris. Fusce fermentum mi id elementum gravida. ";
 
     public static string GenerateLoremIpsum(int passageCount)
     {
-        var builder = new System.Text.StringBuilder();
+        string loremIpsum = string.Empty;
         for (int i = 0; i < passageCount; i++)
         {
-            builder.Append(LoremIpsumPassage);
+            loremIpsum += LoremIpsumPassage;
         }
-        return builder.ToString();
+        return loremIpsum;
     }
 }
