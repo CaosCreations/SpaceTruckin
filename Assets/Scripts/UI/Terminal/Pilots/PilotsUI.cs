185483d9ff27fdf4c0c708378d99b459554b04fa
pseudowalrus <61809127+pseudowalrus@users.noreply.github.com>
2021-01-20 17:47:55 +0000

    Remove all hard-coded instances of lorem ipsum

--- Assets/Scripts/UI/Terminal/Pilots/PilotsUI.cs
+++ Assets/Scripts/UI/Terminal/Pilots/PilotsUI.cs
@@ -175,7 +175,7 @@ public class PilotsUI : MonoBehaviour
 
 		if (string.IsNullOrEmpty(pilot.Description))
 		{
-			pilot.Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque tortor dui, elementum eu convallis non, cursus ac dolor. Quisque dictum est quam, et pellentesque velit rutrum eget. Nullam interdum ultricies velit pharetra aliquet. Integer sodales a magna quis ornare. Ut vulputate nibh ipsum. Vivamus tincidunt nec nisi in fermentum. Mauris consequat mi vel odio consequat, eget gravida urna lobortis. Pellentesque eu ipsum consectetur, pharetra nulla in, consectetur turpis. Curabitur ornare eu nisi tempus varius. Phasellus vel ex mauris. Fusce fermentum mi id elementum gravida.";
+			pilot.Description = PlaceholderUtils.GenerateLoremIpsum();
 		}
 
 		builder.AppendLine("Description: " + pilot.Description);
