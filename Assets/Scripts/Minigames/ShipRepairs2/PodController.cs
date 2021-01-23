1c9a96010926140e910ea431db536329745cc52d
Max Hooton <61809127+maxwellpark@users.noreply.github.com>
2021-01-23 18:14:57 +0000

    Rise if mouse held

--- Assets/Scripts/Minigames/ShipRepairs2/PodController.cs
+++ Assets/Scripts/Minigames/ShipRepairs2/PodController.cs
@@ -2,14 +2,30 @@
 
 public class PodController : MonoBehaviour
 {
-    private float yMin = -4f;
-    private float yMax = 4f;
+    private float yMin, yMax;
+    private float screenBorder = 2f;
+    private float flightSpeed = 2f;
     private float fallSpeed = 1f;
+
+    private void Start()
+    {
+        yMin = -Camera.main.orthographicSize/* + screenBorder*/;
+        yMax = Camera.main.orthographicSize - screenBorder;
+        Debug.Log("Y min: " + yMin);
+        Debug.Log("Y max: " + yMax);
+    }
 
     private void Update()
     {
-        float newY = Mathf.Clamp(transform.position.y - fallSpeed * Time.deltaTime, yMin, yMax);
+        float newY;
+        if (Input.GetMouseButton(0))
+        {
+            newY = Mathf.Clamp(transform.position.y + flightSpeed * Time.deltaTime, yMin, yMax);
+        }
+        else
+        {
+            newY = Mathf.Clamp(transform.position.y - fallSpeed * Time.deltaTime, yMin, yMax);
+        }
         transform.position = new Vector3(transform.position.x, newY, transform.position.z);
-
     }
 }
