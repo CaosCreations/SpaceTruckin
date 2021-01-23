350f67e4f4615f23b200c586b7478aadf0282674
Max Hooton <61809127+maxwellpark@users.noreply.github.com>
2021-01-23 17:30:52 +0000

    Make pod fall and clamp position

---
+++ Assets/Scripts/Minigames/ShipRepairs2/PodController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class PodController : MonoBehaviour
+{
+    private float yMin = -4f;
+    private float yMax = 4f;
+    private float fallSpeed = 1f;
+
+    private void Update()
+    {
+        float newY = Mathf.Clamp(transform.position.y - fallSpeed * Time.deltaTime, yMin, yMax);
+        transform.position = new Vector3(transform.position.x, newY, transform.position.z);
+
+    }
+}
