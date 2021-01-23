a04110bb50df1a9a039a8254fefce59115635e21
Max Hooton <61809127+maxwellpark@users.noreply.github.com>
2021-01-23 18:53:02 +0000

    Flip speed based on above/below midpoint

--- Assets/Scripts/Minigames/ShipRepairs2/PodController.cs
+++ Assets/Scripts/Minigames/ShipRepairs2/PodController.cs
@@ -3,29 +3,42 @@
 public class PodController : MonoBehaviour
 {
     private float yMin, yMax;
+    private float midpointY = 0f;
     private float screenBorder = 2f;
-    private float flightSpeed = 2f;
+    private float flightSpeed = 3.5f;
     private float fallSpeed = 1f;
+
+    public Vector3 Position 
+    {
+        get => transform.position; set => transform.position = value;
+    }
 
     private void Start()
     {
-        yMin = -Camera.main.orthographicSize/* + screenBorder*/;
-        yMax = Camera.main.orthographicSize - screenBorder;
+        yMin = -Camera.main.orthographicSize + screenBorder;
+        yMax = Camera.main.orthographicSize/* - screenBorder*/;
         Debug.Log("Y min: " + yMin);
         Debug.Log("Y max: " + yMax);
     }
 
     private void Update()
     {
+        flightSpeed = IsAboveMidpoint() ? flightSpeed : flightSpeed * -1;
+        Debug.Log("Flight speed = " + flightSpeed);
+
         float newY;
         if (Input.GetMouseButton(0))
         {
-            newY = Mathf.Clamp(transform.position.y + flightSpeed * Time.deltaTime, yMin, yMax);
+            newY = Mathf.Clamp(
+                Position.y + flightSpeed * Time.deltaTime, yMin, yMax);
         }
         else
         {
-            newY = Mathf.Clamp(transform.position.y - fallSpeed * Time.deltaTime, yMin, yMax);
+            newY = Mathf.Clamp(
+                Position.y - fallSpeed * Time.deltaTime, yMin, yMax);
         }
-        transform.position = new Vector3(transform.position.x, newY, transform.position.z);
+        Position = new Vector3(Position.x, newY, Position.z);
     }
+
+    private bool IsAboveMidpoint() => Position.y > midpointY;
 }
