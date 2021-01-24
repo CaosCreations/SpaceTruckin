98e90887a90d39f444048bf54e77677610f26b96
pseudowalrus <61809127+pseudowalrus@users.noreply.github.com>
2021-01-24 12:18:47 +0000

    New day report UI master script

---
+++ Assets/Scripts/UI/Terminal/NewDayReportUI/NewDayReportUI.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class NewDayReportUI : MonoBehaviour
+{
+    public GameObject reportCardPrefab;
+    public GameObject reportCardInstance; 
+    private NewDayReportCard reportCard;
+    private int currentReportIndex;
+
+    public Mission CurrentMissionToReport 
+    {
+        get => MissionsManager.Instance.MissionsCompletedToday[currentReportIndex];
+    }
+    public Mission NextMission { get; set; }
+
+    private void Start()
+    {
+        reportCardInstance = Instantiate(reportCardPrefab, transform);
+        currentReportIndex = 0;
+    }
+
+    private void ShowNextReport()
+    {
+        reportCard.ShowReport(CurrentMissionToReport);
+
+        if (currentReportIndex < MissionsManager.Instance.MissionsCompletedToday.Count - 1)
+        {
+            currentReportIndex++;
+        }
+    }
+}
