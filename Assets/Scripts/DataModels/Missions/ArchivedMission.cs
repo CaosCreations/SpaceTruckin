ed3baf82c54356640dc38ad8de5dee0ec22053ce
pseudowalrus <61809127+pseudowalrus@users.noreply.github.com>
2021-01-27 17:40:36 +0000

    Handle loading and saving archived missions

--- Assets/Scripts/DataModels/Missions/ArchivedMission.cs
+++ Assets/Scripts/DataModels/Missions/ArchivedMission.cs
@@ -1,48 +1,54 @@
 ﻿using System.Threading.Tasks;
 using UnityEngine;
 
-public class ArchivedMission
+/// <summary>
+/// This holds data about mission gains/losses.
+/// The fields of this class/subclass will be set 
+/// during the processing of the mission outcomes.
+/// </summary>
+public class ArchivedMission : IDataModel
 {
+    private string fileName;
 
-    public string missionName;
-    public long totalMoneyEarned;
-    public int totalDamageTaken;
-    public int totalFuelLost;
-    public int totalPilotXpGained;
-    public Ship shipUsed;
+    public ArchivedMission(string fileName)
+    {
+        this.fileName = fileName; // pass in missionname and completionnumber 
+    }
 
-    public string MissionName { get => missionName; set => missionName = value; }
-    public long TotalMoneyEarned { get => totalMoneyEarned; set => totalMoneyEarned = value; }
-    public int TotalDamageTaken { get => totalDamageTaken; set => totalDamageTaken = value; }
-    public int TotalPilotXpGained { get => totalPilotXpGained; set => totalPilotXpGained = value; }
-    public int TotalFuelLost { get => totalFuelLost; set => totalFuelLost = value; }
-    public Ship ShipUsed { get => shipUsed; set => shipUsed = value; }
+    [HideInInspector]
+    private ArchivedMissionSaveData saveData;
 
-    //public ArchivedMission(Mission mission)
-    //{
-    //    MissionName = mission.Name;
-    //    TotalMoneyEarned = 
-    //}
+    public static string FOLDER_NAME = "ArchivedMissionSaveData";
+    public string FileName { get => fileName; set => fileName = value; }
 
-    //[HideInInspector]
-    //ArchivedMissionSaveData saveData;
+    public async Task LoadDataAsync()
+    {
+        saveData = await DataModelsUtils.LoadFileAsync<ArchivedMissionSaveData>(FileName, FOLDER_NAME);
+    }
 
+    public void SaveData()
+    {
+        //string fileName = $"MissionName_{CompletionNumber}";
+        DataModelsUtils.SaveFileAsync(FileName, FOLDER_NAME, saveData);
+    }
 
-    //public class ArchivedMissionSaveData
-    //{
-    //    public string missionName;
-    //    public long totalMoneyEarned;
-    //    public int totalDamageTaken;
-    //    public int totalFuelLost;
-    //    public int totalPilotXpGained;
-    //    public Ship shipUsed;
-    //}
+    public class ArchivedMissionSaveData
+    {
+        public string missionName;
+        public int completionNumber; 
+        public long totalMoneyEarned;
+        public int totalDamageTaken;
+        public int totalFuelLost;
+        public int totalPilotXpGained;
+        public Ship shipUsed; // might need to be id (not instance id)
+    }
 
-    //public string MissionName { get => saveData.missionName; set => saveData.missionName = value; }
-    //public long TotalMoneyEarned { get => saveData.totalMoneyEarned; set => saveData.totalMoneyEarned = value; }
-    //public int TotalDamageTaken { get => saveData.totalDamageTaken; set => saveData.totalDamageTaken = value; }
-    //public int TotalPilotXpGained { get => saveData.totalPilotXpGained; set => saveData.totalPilotXpGained = value; }
-    //public int TotalFuelLost { get => saveData.totalFuelLost; set => saveData.totalFuelLost = value; }
-    //public Ship ShipUsed { get => saveData.shipUsed; set => saveData.shipUsed = value; }
+    public string MissionName { get => saveData.missionName; set => saveData.missionName = value; }
+    public int CompletionNumber { get => saveData.completionNumber; set => saveData.completionNumber = value; }
+    public long TotalMoneyEarned { get => saveData.totalMoneyEarned; set => saveData.totalMoneyEarned = value; }
+    public int TotalDamageTaken { get => saveData.totalDamageTaken; set => saveData.totalDamageTaken = value; }
+    public int TotalPilotXpGained { get => saveData.totalPilotXpGained; set => saveData.totalPilotXpGained = value; }
+    public int TotalFuelLost { get => saveData.totalFuelLost; set => saveData.totalFuelLost = value; }
+    public Ship ShipUsed { get => saveData.shipUsed; set => saveData.shipUsed = value; }
 
 }
