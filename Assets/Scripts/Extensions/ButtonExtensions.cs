2ca5fa0cf950784404f25edbd5e9881606e12812
pseudowalrus <61809127+pseudowalrus@users.noreply.github.com>
2021-01-31 17:09:02 +0000

    Button extension for setting colour

--- Assets/Scripts/Extensions/ButtonExtensions.cs
+++ Assets/Scripts/Extensions/ButtonExtensions.cs
@@ -1,8 +1,16 @@
-﻿public static class ButtonExtensions
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public static class ButtonExtensions
 {
-    public static void AddOnClick(this UnityEngine.UI.Button self, UnityEngine.Events.UnityAction callback)
+    public static void AddOnClick(this Button self, UnityEngine.Events.UnityAction callback)
     {
         self.onClick.RemoveAllListeners();
         self.onClick.AddListener(callback);
+    }
+
+    public static void SetColour(this Button self, Color newColour)
+    {
+        self.GetComponent<Image>().color = newColour;
     }
 }
