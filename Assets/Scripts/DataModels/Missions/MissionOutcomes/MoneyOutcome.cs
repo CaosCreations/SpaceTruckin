b99c214bab7ea8ae289848012dfd2587d2e96017
pseudowalrus <61809127+pseudowalrus@users.noreply.github.com>
2021-01-31 10:51:41 +0000

    Remove commented out code

--- Assets/Scripts/DataModels/Missions/MissionOutcomes/MoneyOutcome.cs
+++ Assets/Scripts/DataModels/Missions/MissionOutcomes/MoneyOutcome.cs
@@ -6,19 +6,6 @@ public class MoneyOutcome : MissionOutcome
     [SerializeField] private long moneyMin;
     [SerializeField] private long moneyMax;
 
-    //public MoneyOutcomeSaveData moneySaveData;
-
-    //[System.Serializable]
-    //public class MoneyOutcomeSaveData : MissionOutcomeSaveData
-    //{
-    //    public long moneyReceived;
-    //}
-
-    //public long MoneyReceived
-    //{
-    //    get => moneySaveData.moneyReceived; set => moneySaveData.moneyReceived = value;
-    //}
-
     public override void Process(Mission mission)
     {
         long moneyEarned = (long)Random.Range(moneyMin, moneyMax);
