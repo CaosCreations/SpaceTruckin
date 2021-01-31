9ee5d16e1ad7ff29a4d3713e496fe87a5da05d42
pseudowalrus <61809127+pseudowalrus@users.noreply.github.com>
2021-01-31 17:08:39 +0000

    UI constants file with button colours

---
+++ Assets/Scripts/UI/UIConstants.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIConstants : MonoBehaviour
+{
+    public static Color InactiveTabButtonColour = Color.white;
+    public static Color ActiveTabButtonColour = Color.green; 
+}
