ce7eaa3526b234d9dfb652ade19101413016e880
pseudowalrus <61809127+pseudowalrus@users.noreply.github.com>
2021-02-03 09:33:37 +0000

    Talent base class and implementation skeleton

---
+++ Assets/Scripts/DataModels/Pilots/PilotTalent.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class PilotTalent : Talent
+{
+    [Header("Set in Editor")]
+    public int flatXpIncrease;
+    public float xpMultiplier;
+    public int shipDamageDecrease;
+
+    public override void ApplyTalents(ScriptableObject pilot)
+    {
+        Debug.Log("");
+    }
+
+	public double ApplyXpIncrease(double xpGained)
+	{
+		xpGained += flatXpIncrease;
+		xpGained *= xpMultiplier;
+        return xpGained;
+
+	}
+
+	public int ApplyShipDamageDecrease(int shipDamage)
+	{
+		return shipDamage -= shipDamageDecrease;
+	}
+
+
+
+}
---
+++ Assets/Scripts/DataModels/Talent.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public interface ITalent
+{
+    void ApplyTalents(ScriptableObject target);
+}
+
+public abstract class Talent : ScriptableObject, ITalent
+{
+    protected string TalentName { get; private set; }
+    protected string Description { get; private set; }
+
+    public abstract void ApplyTalents(ScriptableObject target);
+    
+
+}
