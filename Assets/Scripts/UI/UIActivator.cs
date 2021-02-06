93a48ac72fbe048cbbe794f2e481e918bbc99d53
pseudowalrus <61809127+pseudowalrus@users.noreply.github.com>
2021-02-06 02:11:02 +0000

    Update vending scripts

--- Assets/Scripts/UI/UIActivator.cs
+++ Assets/Scripts/UI/UIActivator.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class UIActivator : MonoBehaviour
@@ -8,7 +7,7 @@ public class UIActivator : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.tag == PlayerConstants.objectTag)
+        if(other.CompareTag(PlayerConstants.objectTag))
         {
             UIManager.SetCanInteract(type, true);
         }
@@ -16,7 +15,7 @@ public class UIActivator : MonoBehaviour
 
     private void OnTriggerExit(Collider other)
     {
-        if(other.tag == PlayerConstants.objectTag)
+        if(other.CompareTag(PlayerConstants.objectTag))
         {
             UIManager.SetCanInteract(type, false);
         }
