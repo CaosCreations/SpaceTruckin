b7ffbc38913ef3ec03f993abf5a792d81ba682b0
pseudowalrus <61809127+pseudowalrus@users.noreply.github.com>
2021-02-17 13:16:46 +0000

    Array extension for randomising

---
+++ Assets/Scripts/Extensions/ArrayExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System.Linq;
+using System;
+
+public static class ArrayExtensions
+{
+    public static object[] Shuffle(this object[] self)
+    {
+        Random random = new Random();
+        return self.OrderBy(x => random.Next()).ToArray();
+    }
+}
