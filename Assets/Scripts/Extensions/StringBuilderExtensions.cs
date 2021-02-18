d82ac78756fa1b92962754c9ac305810ffeebd87
Max <61809127+maxwellpark@users.noreply.github.com>
2021-02-18 18:24:36 +0000

    Update UI to handle SO fields with line breaks (#152)
    * Sb extension
    
    * Update existing code to use new methods
    
    * Update new report card

---
+++ Assets/Scripts/Extensions/StringBuilderExtensions.cs
@@ -0,0 +1,9 @@
+﻿using System.Text;
+
+public static class StringBuilderExtensions
+{
+    public static StringBuilder AppendLineWithBreaks(this StringBuilder self, string value)
+    {
+        return self.AppendLine(value.InsertNewLines());
+    }
+}
--- Assets/Scripts/Extensions/StringExtensions.cs
+++ Assets/Scripts/Extensions/StringExtensions.cs
@@ -1,7 +1,9 @@
-﻿public static class StringExtensions
+﻿using System;
+
+public static class StringExtensions
 {
     public static string InsertNewLines(this string self)
     {
-        return self.Replace("<br>", "\n");
+        return self.Replace("<br>", Environment.NewLine);
     }
 }
