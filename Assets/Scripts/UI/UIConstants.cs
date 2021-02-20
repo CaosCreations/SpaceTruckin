dd3d4d7d53029980f617e540f7a7058963d60ae2
pseudowalrus <61809127+pseudowalrus@users.noreply.github.com>
2021-02-20 13:56:38 +0000

    UI constants for ship preview

--- Assets/Scripts/UI/UIConstants.cs
+++ Assets/Scripts/UI/UIConstants.cs
@@ -3,4 +3,11 @@
 public class UIConstants : MonoBehaviour
 {
     public static Color InactiveTabButtonColour = new Color(255, 255, 255);
+
+    public static Vector3 ShipPreviewOffset = new Vector3(-2f, 0f, 0f);
+    public static Vector3 ShipPreviewRotationSpeed = new Vector3(0, 0.15f, 0);
+
+    // Layers 
+    public static int UILayer = 5;
+    public static int UIObjectLayer = 9;
 }
