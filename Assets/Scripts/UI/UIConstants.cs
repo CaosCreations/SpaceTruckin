626d1f5f131b1c2429cb0939eb18866abdd7bc30
Stefano di Pace <63512233+stedipace@users.noreply.github.com>
2021-02-21 20:17:27 -0300

    Correct Colour Palette for UI (#153)
    * UI on correct colour palette
    
    * Added company logo and fixed report card reference
    
    * Inactive tab button colour uses colour palette
    
    * Update remaining buttons' colours
    
    * Updated unread/read message colours
    
    Co-authored-by: Max Hooton <61809127+maxwellpark@users.noreply.github.com>

--- Assets/Scripts/UI/UIConstants.cs
+++ Assets/Scripts/UI/UIConstants.cs
@@ -2,5 +2,5 @@
 
 public class UIConstants : MonoBehaviour
 {
-    public static Color InactiveTabButtonColour = new Color(255, 255, 255);
+    public static Color InactiveTabButtonColour = new Color(0.949f, 0.941f, 0.898f); // Off-white 
 }
