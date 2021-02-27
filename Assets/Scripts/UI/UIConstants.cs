7fe21d3906bfdaab2eb425314fda82ba683c8558
Max Hooton <61809127+maxwellpark@users.noreply.github.com>
2021-02-27 21:59:09 +0000

    Added hex codes for each of the colour palette

--- Assets/Scripts/UI/UIConstants.cs
+++ Assets/Scripts/UI/UIConstants.cs
@@ -2,15 +2,48 @@
 
 public class UIConstants : MonoBehaviour
 {
-    public static Color InactiveTabButtonColour = new Color(0.949f, 0.941f, 0.898f); // Off-white 
+    // Layers 
+    public static int UILayer = 5;
+    public static int UIObjectLayer = 9;
+
+    // Colour palette
+    public static Color SpringWood = new Color(0xf2, 0xf0, 0xe5);
+    public static Color Chatelle = new Color(0xb8, 0xb5, 0xb9);
+    public static Color Mamba = new Color(0x86, 0x81, 0x88);
+    public static Color DarkGrey = new Color(0x45, 0x44, 0x4f);
+    public static Color Martinique = new Color(0xf2, 0xf0, 0xe5);
+    public static Color Shark = new Color(0x21, 0x21, 0x23);
+    public static Color Blackcurrant = new Color(0x35, 0x2b, 0x42);
+    public static Color MulledWine = new Color(0x43, 0x43, 0x6a);
+    public static Color MulledWineShade1 = new Color(0x4b, 0x41, 0x58);
+    public static Color Indigo = new Color(0x4b, 0x80, 0xca); 
+    public static Color Viking = new Color(0x68, 0xc2, 0xd3);
+    public static Color AquaIsland = new Color(0xa2, 0xdc, 0xc7);
+    public static Color Primrose = new Color(0xed, 0xe1, 0x9e);
+    public static Color Whiskey = new Color(0xd3, 0xa0, 0x68);
+    public static Color Matrix = new Color(0xb4, 0x52, 0x52);
+    public static Color SaltBox = new Color(0x6a, 0x53, 0x6e);
+    public static Color SaltBoxShade1 = new Color(0x64, 0x63, 0x65);
+    public static Color SaltBoxShade2 = new Color(0x5f, 0x55, 0x6a);
+    public static Color IronStone = new Color(0x80, 0x49, 0x3a);
+    public static Color Muesli = new Color(0xa7, 0x7b, 0x5b);
+    public static Color Tacha = new Color(0xc2, 0xd3, 0x68);
+    public static Color ChelseaCucumber = new Color(0x8a, 0xb0, 0x60);
+    public static Color CutySark = new Color(0x56, 0x7b, 0x79);
+    public static Color GreyAsparagus = new Color(0x4e, 0x58, 0x4a);
+    public static Color YellowMetal = new Color(0x7b, 0x72, 0x43);
+    public static Color DoubleSpanishWhite = new Color(0xe5, 0xce, 0xb4);
+    public static Color GreenSmoke = new Color(0xb2, 0xb4, 0x7e);
+    public static Color BeautyBush = new Color(0xed, 0xc8, 0xc4);
+    public static Color Viola = new Color(0xcf, 0x8a, 0xcb);
+
+
+    // UI element colours 
+    public static Color InactiveTabButtonColour = SpringWood;
 
     public static Vector3 ShipPreviewOffset = new Vector3(-3f, 0f, 0f);
     public static Vector3 ShipPreviewRotationSpeed = new Vector3(0f, 0.15f, 0f);
     public static float ShipPreviewScaleFactor = 0.85f;
-
-    // Layers 
-    public static int UILayer = 5;
-    public static int UIObjectLayer = 9;
 
     public static string CloseReportButtonText = "Close";
 }
