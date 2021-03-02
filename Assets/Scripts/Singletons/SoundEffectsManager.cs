7d8bd8f923cf2a95c29d65417e472464ed7f02ab
Max Hooton <61809127+maxwellpark@users.noreply.github.com>
2021-03-02 23:08:27 +0000

    Add singleton for sound effects

---
+++ Assets/Scripts/Singletons/SoundEffectsManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public enum SoundEffect
+{
+	Button, None
+}
+
+public class SoundEffectsManager : MonoBehaviour
+{
+	public static SoundEffectsManager Instance { get; private set; }
+
+	public AudioSource buttonSource;
+
+	private void Awake()
+	{
+		if (Instance == null)
+		{
+			Instance = this;
+			DontDestroyOnLoad(gameObject);
+		}
+		else
+		{
+			Destroy(gameObject);
+			return;
+		}
+	}
+
+	public static void PlaySoundEffect(SoundEffect soundEffect)
+    {
+		switch (soundEffect)
+        {
+			case SoundEffect.None:
+				return;
+			case SoundEffect.Button:
+				Instance.buttonSource.Play();
+				break;
+        }
+    }
+}
