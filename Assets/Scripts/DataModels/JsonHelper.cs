6d96c85fdc7248d1f06df4fc772df6709aab9bb1
Max Hooton <61809127+maxwellpark@users.noreply.github.com>
2021-03-21 18:33:56 +0000

    Update JsonHelper.cs

--- Assets/Scripts/DataModels/JsonHelper.cs
+++ Assets/Scripts/DataModels/JsonHelper.cs
@@ -1,36 +1,49 @@
 ﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 
-// Todo: amalgamate this with DataUtils
 public static class JsonHelper
 {
-    public static T[] ArrayFromJson<T>(string json)
-    {
-        Wrapper<T> wrapper = JsonUtility.FromJson<Wrapper<T>>(json);
-        return wrapper.Items;
-    }
-
     public static string ArrayToJson<T>(T[] array)
     {
-        Wrapper<T> wrapper = new Wrapper<T>();
+        ArrayWrapper<T> wrapper = new ArrayWrapper<T>();
         wrapper.Items = array;
         string json = JsonUtility.ToJson(wrapper);
         return json;
     }
 
-    public static string ArrayToJson<T>(T[] array, bool prettyPrint)
+    public static T[] ArrayFromJson<T>(string json)
     {
-        Wrapper<T> wrapper = new Wrapper<T>
+        ArrayWrapper<T> wrapper = JsonUtility.FromJson<ArrayWrapper<T>>(json);
+        return wrapper.Items;
+    }
+
+    public static string ListToJson<T>(List<T> list)
+    {
+        ListWrapper<T> wrapper = new ListWrapper<T>
         {
-            Items = array
+            Items = list
         };
-        return JsonUtility.ToJson(wrapper, prettyPrint);
+        return JsonUtility.ToJson(wrapper);
+    }
+
+    public static List<T> ListFromJson<T>(string json)
+    {
+        ListWrapper<T> wrapper = JsonUtility.FromJson<ListWrapper<T>>(json);
+        return wrapper.Items;
     }
 
     [Serializable]
-    private class Wrapper<T>
+    private class ArrayWrapper<T>
     {
         // Used to wrap the array of objects 
         public T[] Items;
+    }
+
+    [Serializable]
+    private class ListWrapper<T>
+    {
+        // Used to wrap the list of objects 
+        public List<T> Items;
     }
 }
