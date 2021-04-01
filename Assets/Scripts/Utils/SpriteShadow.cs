c68b84949d8644a1a49700574f9a49486ca71169
jiorno <72587410+akimkalsky@users.noreply.github.com>
2021-04-01 20:56:18 -0300

    First step towards organization
    *deleted unused stuff(materials, models, sprites)
    *created folders for each area.
    *cleaned gameobjects

---
+++ Assets/Scripts/Utils/SpriteShadow.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteShadow : MonoBehaviour {
+
+	 public Renderer renderer;
+	 public bool shadowOnly;
+
+	// Use this for initialization
+	void Start () {
+
+		if (renderer == null) Debug.Log("Renderer is empty"); 
+			GetComponent<Renderer>().shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On; 
+			GetComponent<Renderer>().receiveShadows = true;
+			//TEST
+			if(shadowOnly==true)
+			{GetComponent<Renderer>().shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.ShadowsOnly;}
+
+			
+
+
+			
+		
+	}
+	
+	// Update is called once per frame
+	
+
+	
+}
