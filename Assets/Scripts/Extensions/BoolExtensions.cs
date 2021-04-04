edcd9d32fb428936f64f7b320484dc99fdade8c7
Max Hooton <61809127+maxwellpark@users.noreply.github.com>
2021-04-05 00:14:03 +0100

    Player prefs boilerplate. Keys for used UI types

---
+++ Assets/Scripts/Extensions/BoolExtensions.cs
@@ -0,0 +1,7 @@
+﻿public static class BoolExtensions
+{
+    public static int ToInt(this bool self)
+    {
+        return self ? 1 : 0;
+    }
+}
---
+++ Assets/Scripts/Player/PlayerPrefsManager.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+
+public static class PlayerPrefsManager
+{
+    #region UI
+
+    // Used UI keys - to check if the player has used the UI before
+    public static readonly string TerminalIsUsedKey = "TerminalIsUsed";
+    public static readonly string HangarIsUsedKey = "HangarIsUsed";
+    public static readonly string VendingIsUsedKey = "VendingIsUsed";
+    public static readonly string CasetteIsUsedKey = "CasetteIsUsed";
+    public static readonly string NoticeBoardIsUsedKey = "NoticeBoardIsUsed";
+    public static readonly string BedIsUsedKey = "BedIsUsed";
+
+    public static bool GetUsedUIPref(UICanvasType canvasType)
+    {
+        string key = GetUsedUIKeyByType(canvasType);
+        return GetBool(key);
+    }
+
+    public static void SetUsedUIPref(UICanvasType canvasType)
+    {
+        string key = GetUsedUIKeyByType(canvasType);
+        SetBool(key, true);
+    }
+
+    private static string GetUsedUIKeyByType(UICanvasType canvasType)
+    {
+        string key;
+        switch (canvasType)
+        {
+            case UICanvasType.Terminal:
+                key = TerminalIsUsedKey;
+                break;
+            case UICanvasType.Hangar:
+                key = HangarIsUsedKey;
+                break;
+            case UICanvasType.Vending:
+                key = VendingIsUsedKey;
+                break;
+            case UICanvasType.Cassette:
+                key = CasetteIsUsedKey;
+                break;
+            case UICanvasType.NoticeBoard:
+                key = NoticeBoardIsUsedKey;
+                break;
+            case UICanvasType.Bed:
+                key = BedIsUsedKey;
+                break;
+            default:
+                Debug.LogError("Invalid UI type passed to SetUsedUIPref");
+                return default;
+        }
+        return key;
+    }
+
+    #endregion
+
+    private static bool GetBool(string key, int defaultValue = 0)
+    {
+        return PlayerPrefs.GetInt(key, defaultValue) != 0;
+    }
+
+    private static void SetBool(string key, bool value)
+    {
+        PlayerPrefs.SetInt(key, value.ToInt());
+    }
+    
+    private static void SetInt(string key, int value)
+    {
+        PlayerPrefs.SetInt(key, value);
+    }
+}
