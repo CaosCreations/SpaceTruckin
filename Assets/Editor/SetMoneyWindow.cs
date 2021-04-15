65cf7f30648a4b02acb6b7c8df89a64f39a85c45
pseudowalrus <61809127+pseudowalrus@users.noreply.github.com>
2021-04-16 00:45:40 +0100

    Editor input field that sets player money

---
+++ Assets/Editor/SetMoneyWindow.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+public class SetMoneyWindow : EditorWindow
+{
+    public static event Action OnMoneySet;
+    private long amount;
+
+    [MenuItem("Space Truckin/Player/Set Money")]
+    private static void Init()
+    {
+        EditorWindow window = GetWindow(typeof(SetMoneyWindow));
+        window.Show();
+    }
+
+    private void OnGUI()
+    {
+        EditorGUI.DropShadowLabel(
+            new Rect(0, 0, position.width - 40, 20), "Enter amount: ");
+
+        amount = Math.Max(
+            EditorGUI.LongField(new Rect(20, 20, position.width - 40, 20), amount), 0);
+
+        if (GUI.Button(new Rect(20, 40, position.width - 40, 20), "Set Money"))
+        {
+            PlayerEditor.SetMoney(Math.Max(0, amount));
+            OnMoneySet?.Invoke();
+        }
+
+        Repaint();
+    }
+
+
+
+}
