62889e7b7ee91c221c1eaa9ed495bc06c4556747
Max <61809127+maxwellpark@users.noreply.github.com>
2021-04-18 21:07:23 +0100

    Feature/respawn button (#276)

--- Assets/Scripts/Player/PlayerConstants.cs
+++ Assets/Scripts/Player/PlayerConstants.cs
@@ -6,6 +6,15 @@ public class PlayerConstants : MonoBehaviour
     public static KeyCode ActionKey = KeyCode.E;
     public static KeyCode ExitKey = KeyCode.Escape;
     public static KeyCode DropObjectKey = KeyCode.Q;
+    public static KeyCode RespawnKey = KeyCode.R;
 
     public static string PlayerTag = "Player";
+
+    // Movement 
+    public const float WalkSpeed = 3f;
+    public const float RunSpeed = 7f;
+    public const float Gravity = -9.81f;
+    public const float KillFloorHeight = -25;
+    public static Vector3 PlayerResetPosition = new Vector3(210f, 380f, -247f);
+
 }
