fe1d4df8b800cc7bf879f6b785a784f6b4889fab
Max <61809127+maxwellpark@users.noreply.github.com>
2021-05-10 16:38:42 +0100

    Fixed issue where resetting the repairs workstation would deduct tools and repair the ship  (#356)

--- Assets/Scripts/Minigames/ShipRepairs/Indicator.cs
+++ Assets/Scripts/Minigames/ShipRepairs/Indicator.cs
@@ -8,7 +8,7 @@ public class Indicator : MonoBehaviour
     private void Start()
     {
         repairsManager = GetComponentInParent<RepairsManager>();
-        Workstation.OnRotationStopped += DetermineOutcome; 
+        Workstation.OnRotationStoppedByPlayer += DetermineOutcome; 
     }
 
     public void DetermineOutcome()
