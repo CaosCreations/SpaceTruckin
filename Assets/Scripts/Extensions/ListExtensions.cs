19eaed4c604b29112d7684836deea9b72584b746
pseudowalrus <61809127+pseudowalrus@users.noreply.github.com>
2021-05-13 12:31:33 +0100

    Generic extension method to convert List to HashSet

--- Assets/Scripts/Extensions/ListExtensions.cs
+++ Assets/Scripts/Extensions/ListExtensions.cs
@@ -1,9 +1,20 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 public static class ListExtensions
 {
     public static bool IsNullOrEmpty<T>(this List<T> self)
     {
         return self == null || self.Count <= 0;
+    }
+
+    public static HashSet<T> ToHashSet<T>(this List<T> self) 
+    {
+        var hashSet = new HashSet<T>();
+        
+        self = self.Distinct().ToList();
+        self.ForEach(x => hashSet.Add(x));
+
+        return hashSet;
     }
 }
