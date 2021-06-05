b1870a33f4f058f2614ea2e95eb35689c6ece7d8
Max <61809127+maxwellpark@users.noreply.github.com>
2021-06-05 13:50:57 +0100

    Added script for docking ship at first available node.

---
+++ Assets/Editor/HangarEditor.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+public class HangarEditor : MonoBehaviour
+{
+    [MenuItem("Space Truckin/Hangar/Dock Ship at First Available Node")]
+    private static Ship DockShipAtAvailableNode()
+    {
+        try
+        {
+            HangarSlot hangarSlot = HangarManager.HangarSlots
+                .FirstOrDefault(x => x.IsUnlocked && x.Ship == null);
+            
+            Ship ship = default;
+
+            if (hangarSlot != null)
+            {
+                ship = ShipsManager.Instance.Ships.FirstOrDefault(s => s.IsInQueue);
+
+                HangarManager.DockShip(ship, hangarSlot.Node);
+            }
+
+            return ship;
+        }
+        catch (Exception ex)
+        {
+            Debug.LogException(ex);
+            return null;
+        }
+    }
+
+    [MenuItem("Space Truckin/Hangar/Dock Ship at First Available Node and Assign Random Mission")]
+    private static void DockAndAssignMission()
+    {
+        try
+        {
+            Ship ship = DockShipAtAvailableNode();
+
+            Mission mission = MissionsManager.Instance.Missions
+                .FirstOrDefault(x => MissionsManager.GetScheduledMission(x) == null);
+            
+            if (mission != null)
+            {
+                MissionsManager.AddOrUpdateScheduledMission(ship.Pilot, mission);
+            }
+        }
+        catch (Exception ex)
+        {
+            Debug.LogException(ex);
+        }
+    }
+}
