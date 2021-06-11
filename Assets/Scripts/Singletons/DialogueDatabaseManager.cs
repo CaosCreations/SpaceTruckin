bad8d2c17a05c5091279e9d33712c6d50d195dfe
Max <61809127+maxwellpark@users.noreply.github.com>
2021-06-11 19:35:52 +0100

    Dialogue utils - check if actor exists. Log if field being set is null.

---
+++ Assets/Scripts/Singletons/DialogueDatabaseManager.cs
@@ -0,0 +1,47 @@
+﻿using PixelCrushers.DialogueSystem;
+using UnityEngine;
+
+public class DialogueDatabaseManager : MonoBehaviour
+{
+    public static DialogueDatabaseManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+    }
+
+    /// <summary>
+    /// Increase (or decrease if negative) an actor's Fondness field value, 
+    /// i.e. its relationship points.
+    /// </summary>
+    /// <param name="actorName"></param>
+    /// <param name="valueToAdd"></param>
+    public static void AddToActorFondness(string actorName, int valueToAdd)
+    {
+        if (!string.IsNullOrWhiteSpace(actorName))
+        {
+            // Get the value of the fondness field from the actor in the dialogue database.
+            Lua.Result actorFondnessField = DialogueLua.GetActorField(actorName, DialogueConstants.FondnessFieldName);
+
+            if (actorFondnessField.luaValue == null)
+            {
+                Debug.LogError($"Actor '{actorName}' field '{DialogueConstants.FondnessFieldName} does not exist");
+                return;
+            }
+
+            int currentFondness = actorFondnessField.asInt;
+
+            // Set the fondness field with the increased value.
+            DialogueLua.SetActorField(actorName, DialogueConstants.FondnessFieldName, currentFondness + valueToAdd);
+        }
+    }
+}
