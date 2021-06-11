2c36e0d3484779c2dc9fb38dc97dc524d6a0ce38
Max <61809127+maxwellpark@users.noreply.github.com>
2021-06-11 08:19:37 +0100

    Dedicated Lua handling class with function to set actor relationship points

---
+++ Assets/Scripts/Singletons/DialogueManager.cs
@@ -0,0 +1,32 @@
+﻿using PixelCrushers.DialogueSystem;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour
+{
+    public static DialogueManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+    }
+
+    public static int IncreaseActorFondess(string actorName, int valueToAdd)
+    {
+        int currentFondness = DialogueLua.GetActorField(actorName, DialogueConstants.FondnessFieldName).asInt;
+        int newFondness = currentFondness + valueToAdd;
+
+        DialogueLua.SetActorField(actorName, DialogueConstants.FondnessFieldName, newFondness);
+        
+        return newFondness;
+    }
+
+}
