826dd90c785f7b3566d3f899e5d6ada830f5ad15
Max <61809127+maxwellpark@users.noreply.github.com>
2021-06-19 16:08:36 +0100

    Set field that tracks if convo has completed today. Reset all actor fields on new day.

--- Assets/Scripts/Dialogue/DialogueUtils.cs
+++ Assets/Scripts/Dialogue/DialogueUtils.cs
@@ -1,9 +1,15 @@
-﻿using PixelCrushers.DialogueSystem;
+﻿using Language.Lua;
+using PixelCrushers.DialogueSystem;
 
 public static class DialogueUtils
 {
     public static bool ActorExists(string actorName)
     {
         return DialogueLua.DoesTableElementExist("Actor", actorName);
+    }
+
+    public static LuaTable GetActorsTable()
+    {
+        return Lua.Environment.GetValue("Actor") is LuaTable actorsTable ? actorsTable : null;
     }
 }
--- Assets/Scripts/Singletons/DialogueDatabaseManager.cs
+++ Assets/Scripts/Singletons/DialogueDatabaseManager.cs
@@ -1,4 +1,5 @@
-﻿using PixelCrushers.DialogueSystem;
+﻿using Language.Lua;
+using PixelCrushers.DialogueSystem;
 using UnityEngine;
 
 public class DialogueDatabaseManager : MonoBehaviour
@@ -19,6 +20,11 @@ public class DialogueDatabaseManager : MonoBehaviour
         }
     }
 
+    private void Start()
+    {
+        CalendarManager.OnEndOfDay += ResetHasCompletedConversationsToday;
+    }
+
     /// <summary>
     /// Increase (or decrease if negative) an actor's Fondness field value, 
     /// i.e. its relationship points.
@@ -34,14 +40,55 @@ public class DialogueDatabaseManager : MonoBehaviour
 
             if (actorFondnessField.luaValue == null)
             {
-                Debug.LogError($"Actor '{actorName}' field '{DialogueConstants.FondnessFieldName} does not exist");
+                Debug.LogError(
+                    $"Actor '{actorName}' field '{DialogueConstants.FondnessFieldName} does not exist");
+
                 return;
             }
 
             int currentFondness = actorFondnessField.asInt;
 
             // Set the fondness field with the increased value.
-            DialogueLua.SetActorField(actorName, DialogueConstants.FondnessFieldName, currentFondness + valueToAdd);
+            DialogueLua.SetActorField(
+                actorName, DialogueConstants.FondnessFieldName, currentFondness + valueToAdd);
+        }
+    }
+
+    /// <summary>
+    /// Sets a field on an actor that tracks whether their conversation has been completed today. 
+    /// </summary>
+    /// <param name="hasCompleted"></param>
+    public static void SetHasCompletedConversationToday(string actorName, bool hasCompleted)
+    {
+        Lua.Result hasCompletedField = DialogueLua.GetActorField(
+            actorName, DialogueConstants.HasCompletedConversationTodayFieldName);
+
+        if (hasCompletedField.luaValue == null)
+        {
+            Debug.LogError(
+                $"Actor '{actorName}' field '{DialogueConstants.HasCompletedConversationTodayFieldName} does not exist");
+
+            return;
+        }
+
+        DialogueLua.SetActorField(
+            actorName, DialogueConstants.HasCompletedConversationTodayFieldName, hasCompleted);
+    }
+
+    /// <summary>
+    /// Resets the field on each actor so that on the next day, the conversation completed condition will be met
+    /// until a another conversation is completed that day. 
+    /// </summary>
+    private static void ResetHasCompletedConversationsToday()
+    {
+        // Get all actors in the dialogue database. 
+        LuaTable actorsTable = DialogueUtils.GetActorsTable();
+
+        foreach (var actorKey in actorsTable.Dict.Keys)
+        {
+            // Set each actor's conversation completed field to false. 
+            DialogueLua.SetActorField(
+                actorKey.ToString(), DialogueConstants.HasCompletedConversationTodayFieldName, false);
         }
     }
 }
