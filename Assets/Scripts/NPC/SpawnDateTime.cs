7d755eb3a72906fe4b4947c6c1727eb26938efc3
Max <61809127+maxwellpark@users.noreply.github.com>
2021-06-26 14:16:59 +0100

    Add comments

--- Assets/Scripts/NPC/SpawnDateTime.cs
+++ Assets/Scripts/NPC/SpawnDateTime.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 
+/// <summary>
+/// Each NPC spawn has a time at which they will spawn 
+/// and a time at which they will despawn again.
+/// </summary>
 [Serializable]
 public class SpawnDateTime
 {
