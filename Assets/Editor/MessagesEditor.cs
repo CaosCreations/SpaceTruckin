452bfd18c2eab15d92a8eb7d8b0bd6cdb2c199aa
Max <61809127+maxwellpark@users.noreply.github.com>
2021-06-26 17:08:03 +0100

    Added messages editor scripts - delete all save data, unlock all, read all

---
+++ Assets/Editor/MessagesEditor.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+public class MessagesEditor : MonoBehaviour
+{
+    [MenuItem("Space Truckin/Messages/Unlock All")]
+    private static void UnlockAll()
+    {
+        try
+        {
+            var messageContainer = EditorHelper.GetAsset<MessageContainer>();
+            foreach (var message in messageContainer.messages)
+            {
+                message.IsUnlocked = true;
+            }
+        }
+        catch (Exception ex)
+        {
+            Debug.LogException(ex);
+        }
+    }
+
+    [MenuItem("Space Truckin/Messages/Read All")]
+    private static void ReadAll()
+    {
+        try
+        {
+            var messageContainer = EditorHelper.GetAsset<MessageContainer>();
+            foreach (var message in messageContainer.messages)
+            {
+                message.HasBeenRead = true;
+            }
+        }
+        catch (Exception ex)
+        {
+            Debug.LogException(ex);
+        }
+    }
+
+    public static void DeleteSaveData()
+    {
+        var messageContainer = EditorHelper.GetAsset<MessageContainer>();
+        foreach (var message in messageContainer.messages)
+        {
+            SaveDataEditor.NullifyFields(message.saveData);
+        }
+    }
+}
