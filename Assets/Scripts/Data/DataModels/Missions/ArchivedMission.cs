6d1bad9490d509f88bdd5d45798da65694657666
Max <61809127+maxwellpark@users.noreply.github.com>
2021-06-27 19:04:01 +0000

    Rework Archived Mission system to support Scheduled Mission models (#441)
    * Rework Archived Mission system to support scheduled mission models
    
    * Make filename and foldername static

--- Assets/Scripts/Data/DataModels/Missions/ArchivedMission.cs
+++ Assets/Scripts/Data/DataModels/Missions/ArchivedMission.cs
@@ -1,85 +1,38 @@
-﻿using System.Threading.Tasks;
-using UnityEngine;
-
-/// <summary>
-/// This model holds data about past missions.
+﻿/// <summary>
+/// This class holds data about past missions.
 /// The fields of this class will be set 
 /// during the processing of the mission outcomes.
 /// </summary>
-public class ArchivedMission : IDataModel
+[System.Serializable]
+public class ArchivedMission
 {
-    /// <summary>
-    /// We set the file name based on the mission's name and its completion number.
-    /// e.g. 'Smash_69' would be the 69th completion of the mission 'Smash.' 
-    /// </summary>
-    public string FileName { get; private set; }
-    public static string FOLDER_NAME { get; } = "ArchivedMissionSaveData";
+    public string MissionName;
+    public int CompletionNumber, TotalDamageTaken, TotalDamageReduced, TotalFuelLost;
+    public long TotalMoneyEarned, TotalMoneyIncrease;
+    public double TotalPilotXpGained, TotalXpIncreaseFromLicences;
+    public Pilot Pilot;
+    public int PilotLevelAtTimeOfMission;
+    public int MissionsCompletedByPilotAtTimeOfMission;
+    public Date CompletionDate;
 
-    [HideInInspector]
-    private ArchivedMissionSaveData saveData;
+    /// <summary>Keeps track of whether the mission has been shown in the new day report.</summary>
+    public bool HasBeenViewedInReport;
 
     public ArchivedMission(Mission mission, Pilot pilot, int completionNumber)
     {
-        saveData = new ArchivedMissionSaveData();
         MissionName = mission.Name;
         CompletionNumber = completionNumber;
-        FileName = $"{mission.name}_{completionNumber}";
         Pilot = pilot;
         TotalFuelLost = mission.FuelCost;
         CompletionDate = CalendarManager.Instance.CurrentDate;
     }
 
-    /// <summary>
-    /// We are only interested in storing the totals.
-    /// We can look up the rest of the mission fields 
-    /// by the mission's name if needed.
-    /// </summary>
-    [System.Serializable]
-    public class ArchivedMissionSaveData
+    #region Persistence
+    public static string FILE_NAME = "ArchivedMissionSaveData";
+    public static string FOLDER_NAME = "ArchivedMissionSaveData";
+    public static string FILE_PATH
     {
-        public string missionName;
-        public int completionNumber, totalDamageTaken, totalDamageReduced, totalFuelLost;
-        public long totalMoneyEarned, totalMoneyIncrease;
-        public double totalPilotXpGained, totalXpIncreaseFromLicences;
-        public Pilot pilot;
-        public int pilotLevelAtTimeOfMission;
-        public int missionsCompletedByPilotAtTimeOfMission;
-        public Date completionDate;
+        get => DataUtils.GetSaveFilePath(FOLDER_NAME, FILE_NAME);
     }
-
-    public async Task LoadDataAsync()
-    {
-        saveData = await DataUtils.LoadFileAsync<ArchivedMissionSaveData>(FileName, FOLDER_NAME);
-    }
-
-    public void SaveData()
-    {
-        DataUtils.SaveFileAsync(FileName, FOLDER_NAME, saveData);
-    }
-
-    public string MissionName { get => saveData.missionName; set => saveData.missionName = value; }
-    public int CompletionNumber { get => saveData.completionNumber; set => saveData.completionNumber = value; }
-    public long TotalMoneyEarned { get => saveData.totalMoneyEarned; set => saveData.totalMoneyEarned = value; }
-    public long TotalMoneyIncrease { get => saveData.totalMoneyIncrease; set => saveData.totalMoneyIncrease = value; }
-    public int TotalDamageTaken { get => saveData.totalDamageTaken; set => saveData.totalDamageTaken = value; }
-    public int TotalDamageReduced { get => saveData.totalDamageReduced; set => saveData.totalDamageReduced = value; }
-    public int PilotLevelAtTimeOfMission
-    {
-        get => saveData.pilotLevelAtTimeOfMission; set => saveData.pilotLevelAtTimeOfMission = value;
-    }
-    public int MissionsCompletedByPilotAtTimeOfMission
-    {
-        get => saveData.missionsCompletedByPilotAtTimeOfMission;
-        set => saveData.missionsCompletedByPilotAtTimeOfMission = value;
-    }
-    public double TotalPilotXpGained { get => saveData.totalPilotXpGained; set => saveData.totalPilotXpGained = value; }
-    public double TotalXpIncreaseFromLicences
-    {
-        get => saveData.totalXpIncreaseFromLicences;
-        set => saveData.totalXpIncreaseFromLicences = value;
-    }
-    public int TotalFuelLost { get => saveData.totalFuelLost; set => saveData.totalFuelLost = value; }
-    public Pilot Pilot { get => saveData.pilot; set => saveData.pilot = value; }
-    public Ship Ship => Pilot.Ship;
-    public Date CompletionDate { get => saveData.completionDate; set => saveData.completionDate = value; }
+    #endregion Persistence
 }
