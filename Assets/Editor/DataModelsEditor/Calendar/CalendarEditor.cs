cef5ed0038aa7135dd4de2187bb032bac4c7d054
Max <61809127+maxwellpark@users.noreply.github.com>
2021-07-05 13:24:58 +0100

    Script for creating the window UI and input fields for setting Date

---
+++ Assets/Editor/DataModelsEditor/Calendar/CalendarEditor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+public class CalendarEditor : MonoBehaviour
+{
+    public static void SetDate(Date date)
+    {
+        try
+        {
+            var calendarData = EditorHelper.GetAsset<CalendarData>();
+            calendarData.CurrentDate = date;
+
+            Debug.Log("Current date set to: " + calendarData.CurrentDate.ToString());
+        }
+        catch (Exception ex)
+        {
+            Debug.LogException(ex);
+        }
+    }
+
+    public static void DeleteSaveData()
+    {
+        var calendarData = EditorHelper.GetAsset<CalendarData>();
+        SaveDataEditor.DefaultStructFields(ref calendarData.CurrentDate);
+    }
+}
