a4c62e45c80bcafead3a61044cc2139305d673ab
Max <61809127+maxwellpark@users.noreply.github.com>
2021-07-29 19:18:23 +0100

    Remove redundant attribute

--- Assets/Scripts/Data/DataModels/Missions/Mission.cs
+++ Assets/Scripts/Data/DataModels/Missions/Mission.cs
@@ -19,7 +19,6 @@ public partial class Mission : ScriptableObject, IDataModel
     [SerializeField] private bool hasRandomOutcomes;
     [SerializeField] private MissionOutcome[] outcomes;
     [SerializeField] private MissionModifier missionModifier;
-    [SerializeField] private MissionBonus missionBonus;
 
     [Tooltip("The number of relationship points with the customer awarded on first completion of the mission")]
     [SerializeField] private int fondnessGranted;
@@ -35,7 +34,6 @@ public partial class Mission : ScriptableObject, IDataModel
     [Header("Data to update IN GAME")]
     public MissionSaveData saveData;
 
-    [HideInInspector]
     private ArchivedMission missionToArchive;
 
     public const string FolderName = "MissionSaveData";
