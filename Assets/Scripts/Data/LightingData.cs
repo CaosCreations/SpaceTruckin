b76c708ba391244aed8859ee3164670ce3e48277
Max <61809127+maxwellpark@users.noreply.github.com>
2021-08-13 23:44:59 +0100

    Lighting singleton and SO data class

---
+++ Assets/Scripts/Data/LightingData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "LightingData", menuName = "ScriptableObjects/LightingData", order = 1)]
+public class LightingData : ScriptableObject
+{
+    [SerializeField] private TimeOfDay lightsOutTime;
+    public TimeSpan LightsOutTime;
+
+    [field: SerializeField] public float DayTimeIntensity { get; private set; }
+    [field: SerializeField] public float NightTimeIntensity { get; private set; }
+    [field: SerializeField] public Light StationLight { get; private set; }
+
+    private void OnValidate()
+    {
+        LightsOutTime = lightsOutTime.ToTimeSpan();
+    }
+}
---
+++ Assets/Scripts/Singletons/LightingManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using UnityEngine;
+
+public class LightingManager : MonoBehaviour
+{
+    public static LightingManager Instance { get; private set; }
+
+    [SerializeField] private LightingData lightingData;
+
+    #region Property Accessors 
+    public static TimeSpan LightsOutTime => Instance.lightingData.LightsOutTime;
+    #endregion
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        CalendarManager.OnEndOfDay += TurnOnTheLights;
+    }
+
+    private static void SetLightIntensity(Light light, float intensity)
+    {
+        light.intensity = intensity;
+    }
+
+    public static void TurnOnTheLights()
+    {
+        SetLightIntensity(Instance.lightingData.StationLight, Instance.lightingData.DayTimeIntensity);
+    }
+
+    public static void DimTheLights()
+    {
+        SetLightIntensity(Instance.lightingData.StationLight, Instance.lightingData.NightTimeIntensity);
+    }
+}
