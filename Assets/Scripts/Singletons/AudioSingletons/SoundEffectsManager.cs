86ac8d3db744a07b00d5b9f3236d4ea7a4edc586
Max <61809127+maxwellpark@users.noreply.github.com>
2021-08-22 14:10:40 +0100

    Derive sound effects manager

---
+++ Assets/Scripts/Singletons/AudioSingletons/SoundEffectsManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public enum UISoundEffectType
+{
+    Confirm, Back, Error, Select, SelectMechanical
+}
+
+public class SoundEffectsManager : AudioManager
+{
+    // Audio clips for UI 
+    [SerializeField] private AudioClip confirmClip;
+    [SerializeField] private AudioClip backClip;
+    [SerializeField] private AudioClip errorClip;
+    [SerializeField] private AudioClip selectClip;
+    [SerializeField] private AudioClip selectMechancialClip;
+
+
+}
