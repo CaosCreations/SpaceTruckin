e4e06e39729d4475ed6a753559a64d4877fff5df
Max <61809127+maxwellpark@users.noreply.github.com>
2021-08-31 11:25:16 +0100

    Update UI and ambient managers to use new base

--- Assets/Scripts/Singletons/AudioSingletons/AmbientSoundEffectManager.cs
+++ Assets/Scripts/Singletons/AudioSingletons/AmbientSoundEffectManager.cs
@@ -5,48 +5,22 @@ public enum AmbientSoundEffect
     AsteroidAir
 }
 
-public class AmbientSoundEffectManager : AudioManager, ISoundEffectManager<AmbientSoundEffect>
+public class AmbientSoundEffectManager : SoundEffectsManager<AmbientSoundEffectManager, AmbientSoundEffect>
 {
-    public static AmbientSoundEffectManager Instance { get; private set; }
-
     #region AudioClips
     [SerializeField] private AudioClip asteroidAirClip;
     #endregion
-
-    private void Awake()
-    {
-        if (Instance == null)
-        {
-            Instance = this;
-            DontDestroyOnLoad(gameObject);
-
-            if (audioSource == null)
-            {
-                Debug.LogError($"Audio source in {nameof(AmbientSoundEffectManager)} not found");
-            }
-        }
-        else
-        {
-            Destroy(gameObject);
-            return;
-        }
-    }
 
     private void Start()
     {
         Instance.PlaySoundEffect(AmbientSoundEffect.AsteroidAir);
     }
 
-    public void PlaySoundEffect(AmbientSoundEffect effectType)
-    {
-        Instance.PlayAudioClip(GetClipBySoundEffectType(effectType));
-    }
-
-    public AudioClip GetClipBySoundEffectType(AmbientSoundEffect effectType)
+    public override AudioClip GetClipBySoundEffectType(AmbientSoundEffect effectType)
     {
         return effectType switch
         {
-            AmbientSoundEffect.AsteroidAir => Instance.asteroidAirClip,
+            AmbientSoundEffect.AsteroidAir => asteroidAirClip,
             _ => default,
         };
     }
--- Assets/Scripts/Singletons/AudioSingletons/UISoundEffectManager.cs
+++ Assets/Scripts/Singletons/AudioSingletons/UISoundEffectManager.cs
@@ -5,10 +5,8 @@ public enum UISoundEffect
     Confirm, Back, Error, Select, SelectMechanical
 }
 
-public class UISoundEffectManager : AudioManager, ISoundEffectManager<UISoundEffect>
+public class UISoundEffectManager : SoundEffectsManager<UISoundEffectManager, UISoundEffect>
 {
-    public static UISoundEffectManager Instance { get; private set; }
-
     #region AudioClips
     [SerializeField] private AudioClip confirmClip;
     [SerializeField] private AudioClip backClip;
@@ -17,41 +15,16 @@ public class UISoundEffectManager : AudioManager, ISoundEffectManager<UISoundEff
     [SerializeField] private AudioClip selectMechancialClip;
     #endregion
 
-    private void Awake()
-    {
-        if (Instance == null)
-        {
-            Instance = this;
-            DontDestroyOnLoad(gameObject);
-
-            if (audioSource == null)
-            {
-                Debug.LogError($"Audio source in {nameof(UISoundEffectManager)} not found");
-            }
-        }
-        else
-        {
-            Destroy(gameObject);
-            return;
-        }
-    }
-
-    public void PlaySoundEffect(UISoundEffect effectType)
-    {
-        Instance.PlayAudioClip(GetClipBySoundEffectType(effectType));
-    }
-
-    public AudioClip GetClipBySoundEffectType(UISoundEffect effectType)
+    public override AudioClip GetClipBySoundEffectType(UISoundEffect effectType)
     {
         return effectType switch
         {
-            UISoundEffect.Confirm => Instance.confirmClip,
-            UISoundEffect.Back => Instance.backClip,
-            UISoundEffect.Error => Instance.errorClip,
-            UISoundEffect.Select => Instance.selectClip,
-            UISoundEffect.SelectMechanical => Instance.selectMechancialClip,
+            UISoundEffect.Confirm => confirmClip,
+            UISoundEffect.Back => backClip,
+            UISoundEffect.Error => errorClip,
+            UISoundEffect.Select => selectClip,
+            UISoundEffect.SelectMechanical => selectMechancialClip,
             _ => default,
         };
     }
-
 }
