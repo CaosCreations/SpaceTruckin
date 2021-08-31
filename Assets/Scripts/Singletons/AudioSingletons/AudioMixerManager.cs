6eba9a422b2b887b40f9cd1a3a066c5bdca3840a
Max <61809127+maxwellpark@users.noreply.github.com>
2021-08-31 23:10:46 +0100

    Expose mixer params and method to set them

--- Assets/Scripts/Singletons/AudioSingletons/AudioMixerManager.cs
+++ Assets/Scripts/Singletons/AudioSingletons/AudioMixerManager.cs
@@ -1,4 +1,40 @@
-﻿public class AudioMixerManager : Singleton<AudioMixerManager>
-{
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
 
+public enum MixerGroup
+{
+    Master, Music, SoundEffects
+}
+
+public class AudioMixerManager : Singleton<AudioMixerManager>
+{
+    [SerializeField] private AudioMixer audioMixer;
+
+    #region Mixer Groups
+    [SerializeField] private AudioMixerGroup masterGroup;
+    [SerializeField] private AudioMixerGroup musicGroup;
+    [SerializeField] private AudioMixerGroup soundEffectsGroup;
+    #endregion
+
+    #region Mappings
+    private static readonly Dictionary<MixerGroup, string> VolumeParameterMap = new Dictionary<MixerGroup, string>
+    {
+        { MixerGroup.Master, "MasterVolume" },
+        { MixerGroup.Music, "MusicVolume" },
+        { MixerGroup.SoundEffects, "SoundEffectsVolume" }
+    };
+    #endregion 
+
+    public void SetMixerGroupVolume(MixerGroup mixerGroup, float value)
+    {
+        if (!VolumeParameterMap.ContainsKey(mixerGroup))
+        {
+            Debug.LogError($"MixerGroup parameter {nameof(mixerGroup)} is not mapped");
+        }
+        else
+        {
+            audioMixer.SetFloat(VolumeParameterMap[mixerGroup], value);
+        }
+    }
 }
