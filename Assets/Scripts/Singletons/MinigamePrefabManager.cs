15be848322b6abc58437c4383d3a5cdce2d3c866
Max <61809127+maxwellpark@users.noreply.github.com>
2021-09-05 15:23:15 +0100

    Support instantiating minigame UI prefabs alongside

--- Assets/Scripts/Singletons/MinigamePrefabManager.cs
+++ Assets/Scripts/Singletons/MinigamePrefabManager.cs
@@ -11,59 +11,110 @@ public class MinigamePrefabManager : MonoBehaviour
     public static MinigamePrefabManager Instance { get; private set; }
 
     #region Prefab GameObjects
+    // Gameplay 
     [SerializeField] private GameObject wheelMinigamePrefab;
     [SerializeField] private GameObject stackMinigamePrefab;
 
     private GameObject wheelMinigameInstance;
     private GameObject stackMinigameInstance;
+
+    // UI  
+    [SerializeField] private GameObject wheelMinigameUIPrefab;
+    [SerializeField] private GameObject stackMinigameUIPrefab;
+
+    private GameObject wheelMinigameUIInstance;
+    private GameObject stackMinigameUIInstance;
+
     #endregion
 
     #region Mapping
-    private readonly Dictionary<MinigamePrefab, GameObject> PrefabMap =
+    private readonly Dictionary<MinigamePrefab, GameObject> prefabMap =
         new Dictionary<MinigamePrefab, GameObject>();
 
-    private readonly Dictionary<MinigamePrefab, GameObject> InstanceMap =
+    private readonly Dictionary<MinigamePrefab, GameObject> instanceMap =
+        new Dictionary<MinigamePrefab, GameObject>();
+
+    private readonly Dictionary<MinigamePrefab, GameObject> uiPrefabMap =
+    new Dictionary<MinigamePrefab, GameObject>();
+
+    private readonly Dictionary<MinigamePrefab, GameObject> uiInstanceMap =
         new Dictionary<MinigamePrefab, GameObject>();
 
     private void MapPrefabs()
     {
-        if (wheelMinigamePrefab && !PrefabMap.ContainsKey(MinigamePrefab.Wheel))
+        if (wheelMinigamePrefab && !prefabMap.ContainsKey(MinigamePrefab.Wheel))
         {
-            PrefabMap.Add(MinigamePrefab.Wheel, wheelMinigamePrefab);
+            prefabMap.Add(MinigamePrefab.Wheel, wheelMinigamePrefab);
         }
         else
         {
             Debug.LogError($"{nameof(wheelMinigamePrefab)} on {nameof(MinigamePrefabManager)} is null. Attach the reference.");
         }
 
-        if (stackMinigamePrefab && !PrefabMap.ContainsKey(MinigamePrefab.Stack))
+        if (wheelMinigameUIPrefab && !prefabMap.ContainsKey(MinigamePrefab.Wheel))
         {
-            PrefabMap.Add(MinigamePrefab.Stack, stackMinigamePrefab);
+            uiPrefabMap.Add(MinigamePrefab.Wheel, wheelMinigameUIPrefab);
+        }
+        else
+        {
+            Debug.LogError($"{nameof(wheelMinigameUIPrefab)} on {nameof(MinigamePrefabManager)} is null. Attach the reference.");
+        }
+
+        if (stackMinigamePrefab && !prefabMap.ContainsKey(MinigamePrefab.Stack))
+        {
+            prefabMap.Add(MinigamePrefab.Stack, stackMinigamePrefab);
         }
         else
         {
             Debug.LogError($"{nameof(stackMinigamePrefab)} on {nameof(MinigamePrefabManager)} is null. Attach the reference");
         }
+
+        if (wheelMinigameUIPrefab && !prefabMap.ContainsKey(MinigamePrefab.Stack))
+        {
+            uiPrefabMap.Add(MinigamePrefab.Stack, wheelMinigameUIPrefab);
+        }
+        else
+        {
+            Debug.LogError($"{nameof(stackMinigameUIPrefab)} on {nameof(MinigamePrefabManager)} is null. Attach the reference.");
+        }
     }
 
     private void MapInstances()
     {
-        if (!PrefabMap.ContainsKey(MinigamePrefab.Wheel))
+        if (!instanceMap.ContainsKey(MinigamePrefab.Wheel))
         {
-            InstanceMap.Add(MinigamePrefab.Wheel, wheelMinigameInstance);
+            instanceMap.Add(MinigamePrefab.Wheel, wheelMinigameInstance);
         }
         else
         {
             Debug.LogWarning($"{nameof(wheelMinigameInstance)} on {nameof(MinigamePrefabManager)} is already mapped.");
         }
 
-        if (!InstanceMap.ContainsKey(MinigamePrefab.Stack))
+        if (!uiInstanceMap.ContainsKey(MinigamePrefab.Wheel))
         {
-            InstanceMap.Add(MinigamePrefab.Stack, stackMinigameInstance);
+            instanceMap.Add(MinigamePrefab.Wheel, wheelMinigameUIInstance);
+        }
+        else
+        {
+            Debug.LogWarning($"{nameof(wheelMinigameUIInstance)} on {nameof(MinigamePrefabManager)} is already mapped.");
+        }
+
+        if (!instanceMap.ContainsKey(MinigamePrefab.Stack))
+        {
+            instanceMap.Add(MinigamePrefab.Stack, stackMinigameInstance);
         }
         else
         {
             Debug.LogError($"{nameof(stackMinigameInstance)} on {nameof(MinigamePrefabManager)} is already mapped.");
+        }
+
+        if (!uiInstanceMap.ContainsKey(MinigamePrefab.Stack))
+        {
+            instanceMap.Add(MinigamePrefab.Stack, stackMinigameUIInstance);
+        }
+        else
+        {
+            Debug.LogWarning($"{nameof(stackMinigameUIInstance)} on {nameof(MinigamePrefabManager)} is already mapped.");
         }
     }
     #endregion
@@ -88,22 +139,49 @@ public class MinigamePrefabManager : MonoBehaviour
         MapInstances();
     }
 
-    public GameObject InitPrefab(MinigamePrefab minigamePrefab, Transform parent)
+    #region Prefab Instantiation
+    public GameObject InitPrefab(MinigamePrefab prefabType, Transform parent)
     {
         DestroyExistingInstances();
 
-        InstanceMap[minigamePrefab] = Instantiate(PrefabMap[minigamePrefab], parent);
-        return InstanceMap[minigamePrefab];
+        return InitPrefab(prefabType, prefabMap, instanceMap, parent);
+    }
+
+    public GameObject InitUIPrefab(MinigamePrefab prefabType, Transform parent)
+    {
+        DestroyExistingUIInstances();
+
+        return InitPrefab(prefabType, uiPrefabMap, uiInstanceMap, parent);
+    }
+
+    private GameObject InitPrefab(MinigamePrefab prefabType,
+        Dictionary<MinigamePrefab, GameObject> prefabMap,
+        Dictionary<MinigamePrefab, GameObject> instanceMap,
+        Transform parent)
+    {
+        instanceMap[prefabType] = Instantiate(prefabMap[prefabType], parent);
+        return instanceMap[prefabType];
     }
 
     private void DestroyExistingInstances()
     {
-        //wheelMinigameInstance.DestroyIfExists();
-        //stackMinigameInstance.DestroyIfExists();
-
-        foreach (var mapping in InstanceMap)
+        foreach (var mapping in instanceMap)
         {
             mapping.Value.DestroyIfExists();
         }
     }
+
+    private void DestroyExistingUIInstances()
+    {
+        foreach (var mapping in uiInstanceMap)
+        {
+            mapping.Value.DestroyIfExists();
+        }
+    }
+
+    private bool MinigameHasUI(MinigamePrefab prefabType)
+    {
+        return uiInstanceMap.ContainsKey(prefabType);
+    }
+    #endregion
 }
