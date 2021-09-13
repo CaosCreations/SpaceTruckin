cc3eac879426686b954f7873c0fcede36e55debf
Max <61809127+maxwellpark@users.noreply.github.com>
2021-09-13 13:26:38 +0100

    Support multiple ship damage types

---
+++ Assets/Scripts/Data/DataModels/Ships/ShipUtils.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public static class ShipUtils
+{
+    public static ShipDamageType GetRandomDamageType()
+    {
+        return GetRandomEnumElement<ShipDamageType>();
+    }
+
+    private static T GetRandomEnumElement<T>() where T : Enum
+    {
+        T[] possibleValues = (T[])Enum.GetValues(typeof(T));
+
+        T randomElement = possibleValues.GetRandomElement();
+
+        return randomElement;
+    }
+}
