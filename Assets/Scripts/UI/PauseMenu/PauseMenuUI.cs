8dd958a2a090c1a65b178f77aa3bf78def526c8e
Willy <willy.winelover@gmail.com>
2022-01-20 15:08:54 +0100

    Added the main menu button in the pause button

--- Assets/Scripts/UI/PauseMenu/PauseMenuUI.cs
+++ Assets/Scripts/UI/PauseMenu/PauseMenuUI.cs
@@ -1,3 +1,19 @@
+using UnityEngine;
+using UnityEngine.UI;
+
 public class PauseMenuUI : UICanvasBase
 {
+    [SerializeField] private Button MainMenuButton;
+
+    private void Awake()
+    {
+        MainMenuButton.onClick.RemoveAllListeners();
+        MainMenuButton.onClick.AddListener(ShowMainMenuCanvas);
+    }
+
+    private void ShowMainMenuCanvas()
+    {
+        UIManager.ShowCanvas(canvasType: UICanvasType.MainMenu, viaShortcut: false);
+    }
+
 }
