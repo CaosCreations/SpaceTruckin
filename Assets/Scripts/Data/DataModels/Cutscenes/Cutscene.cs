6b0da6d7d44e228daa8cfe0c2f5ad7160b1901ce
Max <61809127+maxwellpark@users.noreply.github.com>
2022-12-11 20:11:27 +0000

    onActiveSceneChanged timeline playable (#620)

---
+++ Assets/Scripts/Data/DataModels/Cutscenes/Cutscene.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.Playables;
+
+[CreateAssetMenu(fileName = "Cutscene", menuName = "ScriptableObjects/Cutscenes/Cutscene", order = 1)]
+public class Cutscene : ScriptableObject
+{
+    public string Name;
+    public string OnSceneLoadName;
+    public PlayableDirector Director;
+}
