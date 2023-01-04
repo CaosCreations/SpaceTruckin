5d7e3436efa25be5280810e4041f96ab94446eaa
Max <61809127+maxwellpark@users.noreply.github.com>
2023-01-04 18:39:03 +0000

    Add example custom SMS dialog UI

---
+++ Assets/Dialogue System Extras/Stefano/Scripts/StefanoDialogueUI.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using PixelCrushers.DialogueSystem;
+
+public class StefanoDialogueUI : SMSDialogueUI
+{
+
+    // Copied from SMSDialogueUI but added:
+    // 1. Use actor's node color for subtitle background.
+    // 2. Don't add bubbles for blank text.
+    protected override void AddMessage(Subtitle subtitle)
+    {
+        var dialogueActor = GetDialogueActor(subtitle);
+        var template = GetTemplate(subtitle, dialogueActor);
+
+        // Set portrait name/image:
+        template.SetContent(subtitle);
+        var actor = DialogueManager.masterDatabase.GetActor(subtitle.speakerInfo.id);
+        template.GetComponentInChildren<Image>().color = Tools.WebColor(actor.LookupValue("NodeColor"));
+        if (template.portraitName.gameObject != null)
+        {
+            template.portraitName.text = subtitle.speakerInfo.Name;
+            template.portraitName.gameObject.SetActive(true);
+        }
+
+        // If text is blank, that's all we do:
+        if (string.IsNullOrWhiteSpace(subtitle.formattedText.text)) return;
+
+        // The rest is just a copy of the original AddMessage() method:
+        var go = Instantiate(template.panel.gameObject) as GameObject;
+        var text = subtitle.formattedText.text;
+        go.name = (text.Length <= 20) ? text : text.Substring(0, Mathf.Min(20, text.Length)) + "...";
+        instantiatedMessages.Add(go);
+        go.transform.SetParent(messagePanel.transform, false);
+        var panel = go.GetComponent<StandardUISubtitlePanel>();
+        if (panel.addSpeakerName)
+        {
+            subtitle.formattedText.text = string.Format(panel.addSpeakerNameFormat, new object[] { subtitle.speakerInfo.Name, subtitle.formattedText.text });
+        }
+        if (dialogueActor != null && dialogueActor.standardDialogueUISettings.setSubtitleColor)
+        {
+            subtitle.formattedText.text = dialogueActor.AdjustSubtitleColor(subtitle);
+        }
+        panel.ShowSubtitle(subtitle);
+        continueButton = panel.continueButton;
+        if (shouldShowContinueButton && !isLoadingGame)
+        {
+            panel.ShowContinueButton();
+        }
+        else
+        {
+            panel.HideContinueButton();
+        }
+        if (isLoadingGame)
+        {
+            var typewriter = panel.GetTypewriter();
+            if (typewriter != null) typewriter.Stop();
+        }
+        if (maxMessages > 0 && instantiatedMessages.Count > maxMessages)
+        {
+            Destroy(instantiatedMessages[0]);
+            instantiatedMessages.RemoveAt(0);
+        }
+        ScrollToBottom(); //--- Now does smooth scroll: StartCoroutine(JumpToBottom());
+
+    }
+}
