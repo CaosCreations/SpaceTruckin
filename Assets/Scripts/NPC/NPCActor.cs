ec77895e8746d1d0f5b323e9d330b28471895d46
Max <61809127+maxwellpark@users.noreply.github.com>
2023-02-10 19:49:58 +0000

    Hide actor usable name/message on use

---
+++ Assets/Scripts/NPC/NPCActor.cs
@@ -0,0 +1,40 @@
+﻿using PixelCrushers.DialogueSystem;
+using UnityEngine;
+
+[RequireComponent(typeof(Usable))]
+public class NPCActor : MonoBehaviour
+{
+    private Usable usable;
+    
+    private void Awake()
+    {
+        usable = GetComponent<Usable>();
+    }
+
+    public void OnUse(Transform player)
+    {
+        Debug.Log($"{gameObject.name} is being used by {player}.");
+        SetUsableContentVisible(false);
+    }
+
+    public void OnConversationEnd(Transform actor)
+    {
+        Debug.Log($"{gameObject.name}'s conversation with {actor} is ending.");
+        SetUsableContentVisible(true);
+    }
+
+    private void SetUsableContentVisible(bool visible)
+    {
+        if (visible)
+        {
+            // Remove overrides if visible 
+            usable.overrideName = string.Empty;
+            usable.overrideUseMessage = string.Empty;
+        }
+        else
+        {
+            usable.overrideName = " ";
+            usable.overrideUseMessage = " ";
+        }
+    }
+}
