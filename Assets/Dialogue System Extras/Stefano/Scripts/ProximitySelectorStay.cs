710c7825365b4a41ded2599b0874fd716320e4ce
Max <61809127+maxwellpark@users.noreply.github.com>
2023-03-11 09:29:45 +0000

    Extend ProximitySelector class with OnTriggerStay

---
+++ Assets/Dialogue System Extras/Stefano/Scripts/ProximitySelectorStay.cs
@@ -0,0 +1,13 @@
+﻿using PixelCrushers.DialogueSystem.Wrappers;
+using UnityEngine;
+
+/// <summary>
+/// Same as the base <see cref="ProximitySelector"/> but does additional check in <see cref="OnTriggerStay(Collider)"/>
+/// </summary>
+public class ProximitySelectorStay : ProximitySelector
+{
+    protected void OnTriggerStay(Collider other)
+    {
+        CheckTriggerEnter(other.gameObject);
+    }
+}
