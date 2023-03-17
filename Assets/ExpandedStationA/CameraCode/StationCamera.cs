9ebda2d18f48a2b898851815fc6e8d1edbcc43d7
Max <61809127+maxwellpark@users.noreply.github.com>
2023-03-17 18:33:34 +0000

    Add cinemachine camera shake and add apply shake to battery slot action (#701)
    * Player camera screenshake script
    
    * Apply shake to perlin noise on cinemachine
    
    * Add station camera component and singleton to manage them
    
    * Add screen shake to battery slot action
    
    * Add tooltips to shake script

---
+++ Assets/ExpandedStationA/CameraCode/StationCamera.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class StationCamera : MonoBehaviour
+{
+    public enum Identifier
+    {
+        Hangar = 0
+    }
+
+    public Identifier CameraIdentifier;
+    private CinemachineCameraShake cameraShake;
+
+    private void Start()
+    {
+        cameraShake = GetComponent<CinemachineCameraShake>();
+    }
+
+    public void ShakeCamera()
+    {
+        if (cameraShake == null)
+        {
+            Debug.LogError(nameof(CinemachineCameraShake) + " component is null. Unable to shake camera with identifier: " + CameraIdentifier);
+            return;
+        }
+
+        cameraShake.Shake();
+    }
+}
---
+++ Assets/Scripts/Player/ScreenShake.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class ScreenShake : MonoBehaviour
+{
+    [SerializeField]
+    private float duration = 1f;
+
+    [SerializeField]
+    private bool start;
+
+    [SerializeField]
+    private AnimationCurve animationCurve;
+
+    private void Update()
+    {
+        if (start)
+        {
+            StartCoroutine(Shake());
+            start = false;
+        }
+    }
+
+    private IEnumerator Shake()
+    {
+        var startPosition = transform.position;
+        var elapsedTime = 0f;
+
+        while (elapsedTime < duration)
+        {
+            elapsedTime += Time.deltaTime;
+            var strength = animationCurve.Evaluate(elapsedTime / duration);
+            transform.position = startPosition + Random.insideUnitSphere * strength;
+            yield return null;
+        }
+
+        transform.position = startPosition;
+    }
+}
