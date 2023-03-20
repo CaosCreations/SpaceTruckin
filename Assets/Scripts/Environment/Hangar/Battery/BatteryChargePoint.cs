5dc6b6a8d8e34a8e277bb870f062a115ef5b6016
Max <61809127+maxwellpark@users.noreply.github.com>
2023-03-20 22:53:50 +0000

    Use coroutine for charging battery duration

--- Assets/Scripts/Environment/Hangar/Battery/BatteryChargePoint.cs
+++ Assets/Scripts/Environment/Hangar/Battery/BatteryChargePoint.cs
@@ -1,22 +1,40 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class BatteryChargePoint : InteractableObject
 {
+    [SerializeField]
+    private float chargeTimeInSeconds = 2f;
+
     private void OnTriggerStay(Collider other)
     {
-        if (Input.GetKey(PlayerConstants.ActionKey) && IsPlayerInteractable)
+        if (Input.GetKeyDown(PlayerConstants.ActionKey) && IsPlayerInteractable)
         {
             BatteryCharging batteryCharging = other.GetComponentInChildren<BatteryCharging>();
 
             if (batteryCharging != null && !batteryCharging.IsCharged)
             {
-                batteryCharging.Charge();
+                StartCoroutine(ChargeBattery(batteryCharging));
             }
         }
+    }
+
+    private IEnumerator ChargeBattery(BatteryCharging batteryCharging)
+    {
+        Debug.Log("Charge battery routine starting...");
+        yield return new WaitForSeconds(chargeTimeInSeconds);
+
+        Debug.Log("Charge battery routine finished.");
+        batteryCharging.Charge();
     }
 
     protected override bool IsIconVisible => 
         IsPlayerInteractable 
         && HangarManager.CurrentBatteryBeingHeld != null 
         && !HangarManager.CurrentBatteryBeingHeld.BatteryCharging.IsCharged;
+
+    private void OnValidate()
+    {
+        chargeTimeInSeconds = Mathf.Max(0f, chargeTimeInSeconds);
+    }
 }
