d4f00ef8796582c1143ff1692e602707e6045a5b
Max <61809127+maxwellpark@users.noreply.github.com>
2023-03-21 14:46:37 +0000

    Battery charger meter (#708)
    * Use coroutine for charging battery duration
    
    * Add AnimationTimeHandler and extension methods
    
    * Add animator controller and sliced sprite bar
    
    * Disable anim loop time
    
    * Adjust clip tail
    
    * Remove unused field

---
+++ Assets/Scripts/Animation/AnimationTimeHandler.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class AnimationTimeHandler : MonoBehaviour
+{
+    [field: SerializeField]
+    public Animator Animator { get; private set; }
+
+    private bool isRecording;
+
+    [SerializeField]
+    private string stateName;
+
+    public UnityAction OnAnimationEnded;
+
+    public void HandleAnimation()
+    {
+        Animator.Play(stateName);
+        isRecording = true;
+        Debug.Log($"Animation from {gameObject.name} has finished. State playing: {stateName}.");
+    }
+
+    private void Start()
+    {
+        Animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (!isRecording)
+            return;
+
+        if (!Animator.IsPlaying(stateName))
+        {
+            Debug.Log($"Animation from {gameObject.name} has finished. State played: {stateName}.");
+            OnAnimationEnded?.Invoke();
+            isRecording = false;
+        }
+    }
+}
