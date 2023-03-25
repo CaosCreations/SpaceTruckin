81c6441688996bc473ee159f516a7b6bd7c10607
Max <61809127+maxwellpark@users.noreply.github.com>
2023-03-25 09:20:01 +0000

    Add tooltips

--- Assets/Scripts/Data/DataModels/NPCs/NPCData.cs
+++ Assets/Scripts/Data/DataModels/NPCs/NPCData.cs
@@ -3,19 +3,19 @@
 [CreateAssetMenu(fileName = "NPCData", menuName = "ScriptableObjects/NPCData", order = 1)]
 public class NPCData : ScriptableObject
 {
-    [Tooltip("Where the NPC appears by default in the morning and evening respectively")]
+    [field: Tooltip("Where the NPC appears by default in the morning and evening respectively")]
     [field: SerializeField]
     public NPCLocation DefaultLocation { get; private set; } = new();
 
-    [Tooltip("Where the NPC appears in the morning and evening on specific dates")]
+    [field: Tooltip("Where the NPC appears in the morning and evening on specific dates")]
     [field: SerializeField]
     public NPCLocationByDateContainer LocationByDateContainer { get; private set; } = new();
 
-    [Tooltip("What animation parameters are active by default in the morning and evening respectively")]
+    [field: Tooltip("What animation parameters are active by default in the morning and evening respectively")]
     [field: SerializeField]
     public NPCAnimationContext DefaultAnimationContext { get; private set; } = new();
 
-    [Tooltip("What animation parameters are active in the morning and evening on specific dates")]
+    [field: Tooltip("What animation parameters are active in the morning and evening on specific dates")]
     [field: SerializeField]
     public NPCAnimationContextByDateContainer AnimationContextByDateContainer { get; private set; } = new();
 
