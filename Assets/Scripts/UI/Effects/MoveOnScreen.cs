286cc6907dd0a7128aa01f849ec599a894e63866
Max <61809127+maxwellpark@users.noreply.github.com>
2023-04-08 23:46:05 +0100

    Segregate enums

--- Assets/Scripts/UI/Effects/MoveOnScreen.cs
+++ Assets/Scripts/UI/Effects/MoveOnScreen.cs
@@ -2,6 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum ScreenMoveSide
+{
+    Left, Right
+}
+
 public class MoveOnScreen : MonoBehaviour
 {
     [SerializeField] private RectTransform uiObject;
@@ -9,40 +14,40 @@ public class MoveOnScreen : MonoBehaviour
     [SerializeField] private float duration = 1f;
     [SerializeField] private Vector2 centeredPercentage = new(0.5f, 0.5f);
     [SerializeField] private Ease easeType = Ease.OutExpo;
-    [SerializeField] private Side side = Side.Left;
+    [SerializeField] private ScreenMoveSide side = ScreenMoveSide.Left;
 
     [SerializeField] private bool startMoveOnScreen;
     [SerializeField] private bool startMoveOffScreen;
 
-    private Dictionary<Side, Vector2> offScreenPositions;
-    private Dictionary<Side, Vector2> onScreenPositions;
+    private Dictionary<ScreenMoveSide, Vector2> offScreenPositions;
+    private Dictionary<ScreenMoveSide, Vector2> onScreenPositions;
 
     private void Awake()
     {
-        offScreenPositions = new Dictionary<Side, Vector2>
+        offScreenPositions = new Dictionary<ScreenMoveSide, Vector2>
         {
-            { Side.Right, new Vector2(canvas.pixelRect.width * 2f, uiObject.anchoredPosition.y) },
-            { Side.Left, new Vector2(-canvas.pixelRect.width * 2f, uiObject.anchoredPosition.y) }
+            { ScreenMoveSide.Right, new Vector2(canvas.pixelRect.width * 2f, uiObject.anchoredPosition.y) },
+            { ScreenMoveSide.Left, new Vector2(-canvas.pixelRect.width * 2f, uiObject.anchoredPosition.y) }
         };
 
-        onScreenPositions = new Dictionary<Side, Vector2>
+        onScreenPositions = new Dictionary<ScreenMoveSide, Vector2>
         {
-            { Side.Right, new Vector2(canvas.pixelRect.width * centeredPercentage.x, canvas.pixelRect.height * centeredPercentage.y) },
-            { Side.Left, new Vector2(-(canvas.pixelRect.width * centeredPercentage.x), canvas.pixelRect.height * centeredPercentage.y) }
+            { ScreenMoveSide.Right, new Vector2(canvas.pixelRect.width * centeredPercentage.x, canvas.pixelRect.height * centeredPercentage.y) },
+            { ScreenMoveSide.Left, new Vector2(-(canvas.pixelRect.width * centeredPercentage.x), canvas.pixelRect.height * centeredPercentage.y) }
         };
 
         startMoveOnScreen = false;
         startMoveOffScreen = false;
     }
 
-    public void MoveObjectOnScreen(Side side)
+    public void MoveObjectOnScreen(ScreenMoveSide side)
     {
         uiObject.anchoredPosition = offScreenPositions[side];
         uiObject.DOAnchorPos(Vector2.zero, duration).SetEase(easeType);
         //uiObject.DOAnchorPos(onScreenPositions[side], duration).SetEase(easeType
     }
 
-    public void MoveObjectOffScreen(Side side)
+    public void MoveObjectOffScreen(ScreenMoveSide side)
     {
         uiObject.DOAnchorPos(offScreenPositions[side], duration).SetEase(easeType);
     }
