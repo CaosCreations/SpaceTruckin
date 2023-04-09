843cdbd381996bab783ac15dab5a38e6ea02b491
Max <61809127+maxwellpark@users.noreply.github.com>
2023-04-09 13:19:06 +0100

    Component for mapping playing single animation clip to generic input source (#731)
    * InputAnimationTrigger and AnimationByInput classes
    
    * Use coroutine instead of AnimationEvent for playing once

---
+++ Assets/Scripts/UI/Components/InputAnimationTrigger.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class InputAnimationTrigger : MonoBehaviour
+{
+    [SerializeField]
+    private AnimationByInput[] animationsByInputs;
+
+    private void Update()
+    {
+        foreach (var inputMap in animationsByInputs)
+        {
+            if (Input.GetKeyDown(inputMap.KeyCode)
+                || (!string.IsNullOrWhiteSpace(inputMap.ButtonName) && Input.GetButtonDown(inputMap.ButtonName)))
+            {
+                inputMap.Animator.PlayOnce(inputMap.PlayClipName, inputMap.ResetClipName);
+            }
+        }
+    }
+}
