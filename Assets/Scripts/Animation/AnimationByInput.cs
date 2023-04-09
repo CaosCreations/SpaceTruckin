a415aa0ee972fa563190e43adb6ec8bced5174b5
Max <61809127+maxwellpark@users.noreply.github.com>
2023-04-09 12:55:14 +0100

    InputAnimationTrigger and AnimationByInput classes

---
+++ Assets/Scripts/Animation/AnimationByInput.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class AnimationByInput
+{
+    [field: SerializeField]
+    public KeyCode KeyCode { get; private set; }
+
+    [field: SerializeField]
+    public string ButtonName { get; private set; }
+
+    [field: SerializeField]
+    public Animator Animator { get; private set; }
+
+    [field: SerializeField]
+    public string PlayClipName { get; private set; }
+
+    [field: SerializeField]
+    public string ResetClipName { get; private set; }
+}
---
+++ Assets/Scripts/UI/Components/InputAnimationTrigger.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Linq;
+using UnityEngine;
+
+public class InputAnimationTrigger : MonoBehaviour
+{
+    [SerializeField]
+    private AnimationByInput[] animationsByInputs;
+
+    private void Awake()
+    {
+        foreach (var inputMap in animationsByInputs)
+        {
+            AddOnAnimationFinishedEvent(inputMap.Animator, inputMap.PlayClipName, inputMap.ResetClipName);
+        }
+    }
+
+    /// <summary>
+    /// Adds an animation event that will fire when the play clip finishes.
+    /// </summary>
+    /// <exception cref="ArgumentException"></exception>
+    private void AddOnAnimationFinishedEvent(UnityEngine.Object animatorObj, string playClipName, string resetClipName)
+    {
+        Animator animator = animatorObj as Animator;
+        AnimationClip clip = animator.runtimeAnimatorController.animationClips.FirstOrDefault(c => c.name == playClipName);
+
+        if (clip == null)
+            throw new ArgumentException("Clip to play doesn't exist on animator: " + playClipName);
+
+        AnimationEvent animEvent = new()
+        {
+            time = clip.length,
+            functionName = "OnAnimationFinished",
+            stringParameter = resetClipName,
+            objectReferenceParameter = animatorObj
+        };
+
+        clip.AddEvent(animEvent);
+    }
+
+    private void Update()
+    {
+        foreach (var inputMap in animationsByInputs)
+        {
+            if (Input.GetKeyDown(inputMap.KeyCode)
+                || (!string.IsNullOrWhiteSpace(inputMap.ButtonName) && Input.GetButtonDown(inputMap.ButtonName)))
+            {
+                inputMap.Animator.Play(inputMap.PlayClipName);
+            }
+        }
+    }
+
+    /// <summary>
+    /// The animation event handler.
+    /// </summary>
+    private void OnAnimationFinished(UnityEngine.Object animatorObj, string resetClipName)
+    {
+        Animator animator = animatorObj as Animator;
+
+        // Stop the current clip
+        animator.enabled = false;
+
+        // Play a different clip
+        animator.enabled = true;
+        animator.Play(resetClipName);
+    }
+}
