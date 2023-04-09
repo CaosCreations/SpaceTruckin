0eead16f934ec3b9c9538f4cd15acdfd63b88763
Max <61809127+maxwellpark@users.noreply.github.com>
2023-04-09 13:16:02 +0100

    Use coroutine instead of AnimationEvent for playing once

--- Assets/Scripts/Extensions/UnityExtensions/AnimatorExtensions.cs
+++ Assets/Scripts/Extensions/UnityExtensions/AnimatorExtensions.cs
@@ -1,4 +1,6 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections;
+using System.Linq;
 using UnityEngine;
 
 public static class AnimatorExtensions
@@ -38,5 +40,42 @@ public static class AnimatorExtensions
     public static bool IsPlaying(this Animator self, string stateName)
     {
         return self.IsPlaying() && self.GetCurrentAnimatorStateInfo(0).IsName(stateName);
+    }
+
+    public static AnimationClip GetClipByName(this Animator self, string clipName)
+    {
+        return self.runtimeAnimatorController.animationClips.FirstOrDefault(c => c.name == clipName);
+    }
+
+    public static void PlayOnce(this Animator self, string playClipName, string resetClipName)
+    {
+        var playClip = self.GetClipByName(playClipName);
+
+        if (playClip == null)
+            throw new ArgumentException("Animation clip doesn't exist with name: " + playClipName);
+
+        // Play the clip to play once
+        //self.CrossFadeInFixedTime(playClipName, 0.2f);
+        self.Play(playClipName);
+
+        var behaviour = self.GetComponent<MonoBehaviour>();
+
+        // Wait for the clip to finish playing
+        behaviour.StartCoroutine(WaitForClipToFinish(playClip.length));
+
+        IEnumerator WaitForClipToFinish(float clipLength)
+        {
+            // Wait until the clip has finished playing
+            var elapsedTime = 0f;
+            while (elapsedTime < clipLength)
+            {
+                elapsedTime += Time.deltaTime;
+                yield return null;
+            }
+
+            // Return to the original clip
+            //self.CrossFadeInFixedTime(resetClipName, 0.2f);
+            self.Play(resetClipName);
+        }
     }
 }
--- Assets/Scripts/UI/Components/InputAnimationTrigger.cs
+++ Assets/Scripts/UI/Components/InputAnimationTrigger.cs
@@ -1,42 +1,9 @@
-﻿using System;
-using System.Linq;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class InputAnimationTrigger : MonoBehaviour
 {
     [SerializeField]
     private AnimationByInput[] animationsByInputs;
-
-    private void Awake()
-    {
-        foreach (var inputMap in animationsByInputs)
-        {
-            AddOnAnimationFinishedEvent(inputMap.Animator, inputMap.PlayClipName, inputMap.ResetClipName);
-        }
-    }
-
-    /// <summary>
-    /// Adds an animation event that will fire when the play clip finishes.
-    /// </summary>
-    /// <exception cref="ArgumentException"></exception>
-    private void AddOnAnimationFinishedEvent(UnityEngine.Object animatorObj, string playClipName, string resetClipName)
-    {
-        Animator animator = animatorObj as Animator;
-        AnimationClip clip = animator.runtimeAnimatorController.animationClips.FirstOrDefault(c => c.name == playClipName);
-
-        if (clip == null)
-            throw new ArgumentException("Clip to play doesn't exist on animator: " + playClipName);
-
-        AnimationEvent animEvent = new()
-        {
-            time = clip.length,
-            functionName = "OnAnimationFinished",
-            stringParameter = resetClipName,
-            objectReferenceParameter = animatorObj
-        };
-
-        clip.AddEvent(animEvent);
-    }
 
     private void Update()
     {
@@ -45,23 +12,8 @@ public class InputAnimationTrigger : MonoBehaviour
             if (Input.GetKeyDown(inputMap.KeyCode)
                 || (!string.IsNullOrWhiteSpace(inputMap.ButtonName) && Input.GetButtonDown(inputMap.ButtonName)))
             {
-                inputMap.Animator.Play(inputMap.PlayClipName);
+                inputMap.Animator.PlayOnce(inputMap.PlayClipName, inputMap.ResetClipName);
             }
         }
-    }
-
-    /// <summary>
-    /// The animation event handler.
-    /// </summary>
-    private void OnAnimationFinished(UnityEngine.Object animatorObj, string resetClipName)
-    {
-        Animator animator = animatorObj as Animator;
-
-        // Stop the current clip
-        animator.enabled = false;
-
-        // Play a different clip
-        animator.enabled = true;
-        animator.Play(resetClipName);
     }
 }
