43a5b7e192ed253fb96f15a13e7caf855c21c26c
Max <61809127+maxwellpark@users.noreply.github.com>
2023-04-11 14:49:18 +0100

    Timeline cutscene configuration updates (#739)
    * Add CutsceneTimelinePlayer class with director ref
    
    * Update binding setting to include all directors
    
    * Support multiple cutscene cameras

---
+++ Assets/Scripts/Animation/Timeline/CutsceneTimelinePlayer.cs
@@ -0,0 +1,15 @@
+﻿using Cinemachine;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class CutsceneTimelinePlayer : MonoBehaviour
+{
+    [field: SerializeField]
+    public PlayableDirector PlayableDirector { get; private set; }
+
+    [field: SerializeField]
+    public Cutscene Cutscene { get; private set; }
+
+    [field: SerializeField]
+    public CinemachineVirtualCamera VirtualCamera { get; private set; }
+}
