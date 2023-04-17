18917669d8c7b8fc3af5f2f2c54f91ca1ceb61ed
Max <61809127+maxwellpark@users.noreply.github.com>
2023-04-17 16:04:06 +0100

    Update DialogueEventManager namespace

--- Assets/Scripts/Singletons/DialogueSingletons/DialogueEventManager.cs
+++ Assets/Scripts/Singletons/DialogueSingletons/DialogueEventManager.cs
@@ -2,41 +2,38 @@
 using PixelCrushers.DialogueSystem;
 using UnityEngine;
 
-namespace Assets.Scripts.Singletons.DialogueSingletons
+public class DialogueEventManager : MonoBehaviour
 {
-    public class DialogueEventManager : MonoBehaviour
+    public static DialogueEventManager Instance { get; private set; }
+
+    private void Awake()
     {
-        public static DialogueEventManager Instance { get; private set; }
-
-        private void Awake()
+        if (Instance != null)
         {
-            if (Instance != null)
-            {
-                Destroy(gameObject);
-                return;
-            }
-            Instance = this;
-            DontDestroyOnLoad(gameObject);
+            Destroy(gameObject);
+            return;
         }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
 
-        private void Start()
-        {
-            RegisterEvents();
-        }
+    private void Start()
+    {
+        RegisterEvents();
+    }
 
-        private void RegisterEvents()
-        {
-            SingletonManager.EventService.Add<OnCutsceneFinishedEvent>(OnCutsceneFinishedHandler);
-        }
+    private void RegisterEvents()
+    {
+        SingletonManager.EventService.Add<OnCutsceneFinishedEvent>(OnCutsceneFinishedHandler);
+    }
 
-        private void OnCutsceneFinishedHandler(OnCutsceneFinishedEvent finishedEvent)
+    private void OnCutsceneFinishedHandler(OnCutsceneFinishedEvent finishedEvent)
+    {
+        if (finishedEvent.Cutscene.ConversationSettings != null
+            && finishedEvent.Cutscene.ConversationSettings.CloseDialogueUIOnStart)
         {
-            if (finishedEvent.Cutscene.ConversationSettings != null
-                && finishedEvent.Cutscene.ConversationSettings.CloseDialogueUIOnStart)
-            {
-                Debug.Log($"Dialogue cutscene finished event call back fired and conversation configured. Starting conversation with name '{finishedEvent.Cutscene.ConversationSettings.ConversationName}'");
-                DialogueManager.StartConversation(finishedEvent.Cutscene.ConversationSettings.ConversationName);
-            }
+            Debug.Log($"Dialogue cutscene finished event call back fired and conversation configured. Starting conversation with name '{finishedEvent.Cutscene.ConversationSettings.ConversationName}'");
+            DialogueManager.StartConversation(finishedEvent.Cutscene.ConversationSettings.ConversationName);
         }
     }
 }
