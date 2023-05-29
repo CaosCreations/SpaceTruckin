5b98b40c51bb3888eb82749df5c1962c84bb2e41
Max <61809127+maxwellpark@users.noreply.github.com>
2023-05-29 16:58:55 +0100

    Add prefab and gizmos

--- Assets/Scripts/Animation/Timeline/CutsceneCollisionTrigger.cs
+++ Assets/Scripts/Animation/Timeline/CutsceneCollisionTrigger.cs
@@ -5,10 +5,14 @@ public class CutsceneCollisionTrigger : MonoBehaviour
 {
     [SerializeField] private Cutscene cutscene;
 
+    private BoxCollider boxCollider;
+
     private void Awake()
     {
         if (cutscene == null)
             throw new NullReferenceException("Cutscene reference missing on CutsceneCollisionTrigger");
+
+        boxCollider = GetComponent<BoxCollider>();
     }
 
     private void OnTriggerEnter(Collider other)
@@ -18,6 +22,15 @@ public class CutsceneCollisionTrigger : MonoBehaviour
             Debug.Log("Player collided with CutsceneCollisionTrigger");
             TimelineManager.PlayCutscene(cutscene);
             gameObject.DestroyIfExists();
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (boxCollider != null)
+        {
+            Gizmos.color = Color.magenta;
+            Gizmos.DrawWireCube(transform.position + boxCollider.center, boxCollider.size);
         }
     }
 }
