e8576830abcbb4fbe2f09311a410ffa54e9abba2
Max <61809127+maxwellpark@users.noreply.github.com>
2023-06-28 20:57:58 +0100

    Main menu button in credits scene

---
+++ Assets/Scripts/UI/MainMenu/CreditsUI.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class CreditsUI : MonoBehaviour
+{
+    [SerializeField]
+    private Button mainMenuButton;
+
+    [SerializeField]
+    private GameObject creditsCanvas;
+
+    [SerializeField] 
+    private GameObject loadingScreenCanvas;
+
+    private Slider loadingBarSlider;
+
+    private void Awake()
+    {
+        loadingScreenCanvas.SetActive(false);
+        loadingBarSlider = loadingScreenCanvas.GetComponentInChildren<Slider>();
+        mainMenuButton.AddOnClick(MainMenuButtonHandler);
+    }
+
+    private void MainMenuButtonHandler()
+    {
+        creditsCanvas.SetActive(false);
+        loadingScreenCanvas.SetActive(true);
+        SceneLoadingManager.Instance.LoadSceneAsync(SceneType.TitleScreen, loadingBarSlider);
+    }
+}
