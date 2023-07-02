3d19859a3b3fd56eb9c5b34b1ba09d4f5c2d201e
Max <61809127+maxwellpark@users.noreply.github.com>
2023-07-02 08:53:57 +0100

    Add pilot button to types and play on click

--- Assets/Scripts/Singletons/UISingletons/UIManager.cs
+++ Assets/Scripts/Singletons/UISingletons/UIManager.cs
@@ -42,7 +42,10 @@ public class UIManager : MonoBehaviour
     [SerializeField]
     private TransitionUI transitionUI;
 
-    private static Type[] SuccessInputTypes => new Type[] { typeof(Button), typeof(Slider), typeof(MissionUIItem) };
+    private static Type[] SuccessInputTypes => new[]
+    {
+        typeof(Button), typeof(Slider), typeof(AudioVolumeSlider), typeof(Scrollbar), typeof(MissionUIItem), typeof(PilotInMissionScheduleSlot)
+    };
     private static bool IsErrorInput => Input.GetMouseButtonDown(0)
         && !UIUtils.IsPointerOverObjectType(SuccessInputTypes)
         && !DialogueUtils.IsConversationActive
