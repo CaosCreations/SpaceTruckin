f973a47c571af7e37308ca14a8fedd01c7ff6f72
Max <61809127+maxwellpark@users.noreply.github.com>
2023-07-26 19:00:20 +0100

    Incorporate time of day phase into station blocking object criteria (#895)

--- Assets/Scripts/Environment/StationBlockingObjectSet.cs
+++ Assets/Scripts/Environment/StationBlockingObjectSet.cs
@@ -5,13 +5,14 @@ using UnityEngine;
 public class StationBlockingObjectSet : MonoBehaviour
 {
     [SerializeField]
-    private Date[] activeDates;
+    private DateWithPhase[] activeDates;
 
     private void Start()
     {
         UpdateBlockingObjects();
         SingletonManager.EventService.Add<OnStationSetUpEvent>(OnStationSetUpHandler);
         SingletonManager.EventService.Add<OnEndOfDayEvent>(OnEndOfDayHandler);
+        SingletonManager.EventService.Add<OnEveningStartEvent>(OnEveningStartHandler);
     }
 
     private void UpdateBlockingObjects()
@@ -19,14 +20,14 @@ public class StationBlockingObjectSet : MonoBehaviour
         var currentDate = CalendarManager.CurrentDate;
 
         // If all active dates have passed, we no longer need this object set
-        if (!activeDates.Any(d => d <= currentDate))
+        if (activeDates.All(d => d.Date > currentDate))
         {
             Debug.Log($"{nameof(StationBlockingObjectSet)} - All active dates passed for {gameObject.name}. Destroying set.");
             Destroy(gameObject);
             return;
         }
 
-        var active = activeDates.Contains(currentDate);
+        var active = activeDates.Any(d => d.Date == CalendarManager.CurrentDate && (d.Phase == ClockManager.CurrentTimeOfDayPhase || d.Phase == TimeOfDay.Phase.Either));
         if (active)
         {
             Debug.Log($"{nameof(StationBlockingObjectSet)} - Setting {gameObject.name} children active.");
@@ -44,11 +45,16 @@ public class StationBlockingObjectSet : MonoBehaviour
         UpdateBlockingObjects();
     }
 
+    private void OnEveningStartHandler()
+    {
+        UpdateBlockingObjects();
+    }
+
     private void OnValidate()
     {
         for (int i = 0; i < activeDates.Length; i++)
         {
-            activeDates[i].Validate();
+            activeDates[i].Date.Validate();
         }
     }
 }
