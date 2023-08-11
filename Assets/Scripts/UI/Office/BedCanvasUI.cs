cde2cb088377b9ae43b3475bca7465101e5a049e
Max <61809127+maxwellpark@users.noreply.github.com>
2023-08-12 00:54:12 +0100

    Cutscene triggers sleep

--- Assets/Scripts/UI/Office/BedCanvasUI.cs
+++ Assets/Scripts/UI/Office/BedCanvasUI.cs
@@ -1,19 +1,34 @@
 ﻿using Events;
+using System.Linq;
 using UnityEngine;
 
 public class BedCanvasUI : UICanvasBase
 {
     [SerializeField] private string popupText = "Time for bed?";
+    [SerializeField] private Cutscene[] endOfDayCutscenes;
 
     private void OnEnable()
     {
-        PopupManager.ShowPopup(OnSleep, () => { }, bodyText: popupText);
+        PopupManager.ShowPopup(Sleep, () => { }, bodyText: popupText);
     }
 
-    private void OnSleep()
+    private void Start()
+    {
+        SingletonManager.EventService.Add<OnCutsceneFinishedEvent>(OnCutsceneFinishedHandler);
+    }
+
+    private void Sleep()
     {
         PlayerManager.EnterPausedState();
         UIManager.ClearCanvases(false);
         SingletonManager.EventService.Dispatch(new OnPlayerSleepEvent());
+    }
+
+    private void OnCutsceneFinishedHandler(OnCutsceneFinishedEvent evt)
+    {
+        if (endOfDayCutscenes != null && endOfDayCutscenes.Contains(evt.Cutscene))
+        {
+            Sleep();
+        }
     }
 }
