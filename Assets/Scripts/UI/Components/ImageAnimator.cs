90d09adff2bf6745c0a0b746694455dcdf675125
Max <61809127+maxwellpark@users.noreply.github.com>
2023-08-26 13:42:09 +0100

    ImageAnimator that swaps between looping and static sprite

---
+++ Assets/Scripts/UI/Components/ImageAnimator.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageAnimator : MonoBehaviour
+{
+    [SerializeField] private bool isLooping;
+    [SerializeField] private string loopStateName = "LoopAnimation";
+    [SerializeField] private Sprite staticSprite; 
+    private Animator animator;
+    private Image image;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        image = GetComponent<Image>();
+        UpdateAnimation();
+    }
+
+    public void UpdateAnimation(bool isLooping)
+    {
+        this.isLooping = isLooping;
+        UpdateAnimation();
+    }
+
+    private void UpdateAnimation()
+    {
+        if (isLooping)
+        {
+            animator.enabled = true;
+            animator.Play(loopStateName);
+        }
+        else
+        {
+            animator.enabled = false;
+            image.sprite = staticSprite;
+        }
+    }
+}
