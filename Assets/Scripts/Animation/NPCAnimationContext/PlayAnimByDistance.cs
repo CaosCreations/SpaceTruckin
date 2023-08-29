a8f501d482dc01c2bf0e460df71ab00521638613
j <72587410+akimkalsky@users.noreply.github.com>
2023-08-29 13:21:27 -0300

    random enviroment stuff
    :bird: :gun:

--- Assets/Scripts/Animation/NPCAnimationContext/PlayAnimByDistance.cs
+++ Assets/Scripts/Animation/NPCAnimationContext/PlayAnimByDistance.cs
@@ -6,8 +6,14 @@ public class PlayAnimByDistance : MonoBehaviour
 {
     public float range;
     private Transform player;
+   // public float playerZ;
+   // public float playerX;
+   public float stDistance;
+
     public Animator myAnimator;
     private bool animationStarted=false;
+
+    public bool zDistance;
 
     void Start()
     {
@@ -16,17 +22,67 @@ public class PlayAnimByDistance : MonoBehaviour
 
     void Update()
     {
-        if (Vector3.Distance(player.position, transform.position) <= range && animationStarted==false)
-        {
-           // if (!GetComponent<Animation>().isPlaying) GetComponent<Animation>().Play();
-           myAnimator.Play("Base Layer.birb001flyLeft",0,0);
-           animationStarted=true;
+        stDistance=  Vector3.Distance(player.position, transform.position);
 
-           
+        //idk if vector distance is a good
+        // I heard from a guy...that is not good for performance.
+
+           if(player.transform.position.z<this.transform.position.z+2 && player.transform.position.z>this.transform.position.z-2)
+       {
+           // myAnimator.Play("Base Layer.birb001flysoutht",0,0);
+          // animationStarted=true;   
+          zDistance=true;
+
+       }else{ zDistance=false;}
+
+
+        if (Vector3.Distance(player.position, transform.position) < range && animationStarted==false && zDistance==true) 
+        {
+         
+      
+     
+       if(player.transform.position.x>this.transform.position.x+2 && animationStarted==false)
+       {
+         myAnimator.Play("Base Layer.birb001flyLeft",0,0);
+           animationStarted=true;   
+
+       }
+
+           if(player.transform.position.x<this.transform.position.x+2 && animationStarted==false)
+       {
+         myAnimator.Play("Base Layer.birb001flyRight",0,0);
+           animationStarted=true;   
+
+       }
+
         }
-       // else
-       // {
-            //if (GetComponent<Animation>().isPlaying) GetComponent<Animation>().Stop();
-       // }
+
+
+//for Z?
+
+   if (Vector3.Distance(player.position, transform.position) < range && animationStarted==false && zDistance==false) 
+        {
+         
+      
+     
+       if(player.transform.position.z>this.transform.position.z+1 && animationStarted==false)
+       {
+         myAnimator.Play("Base Layer.birb001flysoutht",0,0);
+           animationStarted=true;   
+
+       }
+
+           if(player.transform.position.z<this.transform.position.z+1 && animationStarted==false)
+       {
+         myAnimator.Play("Base Layer.birb001flynortht",0,0);
+           animationStarted=true;   
+
+       }
+
+        }
+       
+
+
+
     }
 }
