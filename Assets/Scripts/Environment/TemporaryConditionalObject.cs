0f06ff93ffa9e986bee186561e2602a726bbb771
Max <61809127+maxwellpark@users.noreply.github.com>
2023-10-08 19:15:45 +0100

    Dialogue condition for temporary objects

--- Assets/Scripts/Environment/TemporaryConditionalObject.cs
+++ Assets/Scripts/Environment/TemporaryConditionalObject.cs
@@ -7,22 +7,17 @@ public class TemporaryConditionalObject : MonoBehaviour
 {
     public enum ConditionType
     {
-        Date, Message, Mission
-    }
-
-    public enum Operator
-    {
-        True, False,
+        Date, Message, Mission, DialogueVariable,
     }
 
     [Serializable]
     public class Condition
     {
         public ConditionType Type;
-        public Operator Operator;
         public DateWithPhase[] ActiveDates;
         public Message Message;
         public Mission Mission;
+        public string DialogueVariableName;
     }
 
     [SerializeField]
@@ -34,6 +29,7 @@ public class TemporaryConditionalObject : MonoBehaviour
         SingletonManager.EventService.Add<OnEveningStartEvent>(EveningStartHandler);
         SingletonManager.EventService.Add<OnMessageReadEvent>(MessageReadHandler);
         SingletonManager.EventService.Add<OnMissionCompletedEvent>(MissionCompletedHandler);
+        SingletonManager.EventService.Add<OnConversationEndedEvent>(ConversationEndedHandler);
         SetActive();
     }
 
@@ -44,6 +40,7 @@ public class TemporaryConditionalObject : MonoBehaviour
             ConditionType.Date => c.ActiveDates.Any(d => d.Date == CalendarManager.CurrentDate && d.Phase == ClockManager.CurrentTimeOfDayPhase),
             ConditionType.Message => c.Message.HasBeenRead,
             ConditionType.Mission => c.Mission.HasBeenCompleted,
+            ConditionType.DialogueVariable => DialogueDatabaseManager.GetLuaVariableAsBool(c.DialogueVariableName),
             _ => false
         });
     }
@@ -69,6 +66,11 @@ public class TemporaryConditionalObject : MonoBehaviour
     }
 
     private void MissionCompletedHandler()
+    {
+        SetActive();
+    }
+
+    private void ConversationEndedHandler(OnConversationEndedEvent evt)
     {
         SetActive();
     }
