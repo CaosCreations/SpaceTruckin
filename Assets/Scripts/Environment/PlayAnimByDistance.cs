6881325ac06f62a4925e17fceda9ac337f97ea11
j <72587410+akimkalsky@users.noreply.github.com>
2023-12-02 15:27:13 -0300

    A Clever Approach: How Bird Programming Updated the Space Station Amidst the Plague (#1070)
    * npc face player ( right ) at certain distance
    
    :ski: :facepunch: :frog: :nut_and_bolt: :chestnut: :doughnut:
    
    * Bird Codes
    
    :bird:  :bird:  :bird: :bird: :bird:  :bird:
    :deciduous_tree:  :bowling:
    
    * Enable CTP
    
    ---------
    
    Co-authored-by: Max <61809127+maxwellpark@users.noreply.github.com>

--- Assets/Scripts/Environment/PlayAnimByDistance.cs
+++ Assets/Scripts/Environment/PlayAnimByDistance.cs
@@ -5,99 +5,138 @@ using UnityEngine;
 public class PlayAnimByDistance : MonoBehaviour
 {
     public float range;
+    public float respawDist;
     private Transform player;
-   public int randomAnimState;
+    public int randomAnimState;
 
     public Animator myAnimator;
-    private bool animationStarted=false;
+    private bool animationStarted = false;
 
     public bool zDistance;
+    public float ooverallDist;
 
     void Awake()
     {
         player = GameObject.FindWithTag("Player").GetComponent<Transform>();
         myAnimator = GetComponent<Animator>();
-        randomAnimState= Random.Range(0, 5);
-           //setting up RandomIddle
-        if(randomAnimState==0){myAnimator.Play("Base Layer.birbIdle000",0,0);}
-        if(randomAnimState==1){myAnimator.Play("Base Layer.birbIdle001",0,0);}
-        if(randomAnimState==2){myAnimator.Play("Base Layer.birbIdle002",0,0);}
-        if(randomAnimState==3){myAnimator.Play("Base Layer.birbIdle003",0,0);}
-        if(randomAnimState==4){myAnimator.Play("Base Layer.birbIdle004",0,0);}
-        if(randomAnimState==5){myAnimator.Play("Base Layer.birbIdle005",0,0);}
-        
-        
+        randomAnimState = Random.Range(0, 5);
+        //setting up RandomIddle
+        if (randomAnimState == 0) { myAnimator.Play("Base Layer.birbIdle000", 0, 0); }
+        if (randomAnimState == 1) { myAnimator.Play("Base Layer.birbIdle001", 0, 0); }
+        if (randomAnimState == 2) { myAnimator.Play("Base Layer.birbIdle002", 0, 0); }
+        if (randomAnimState == 3) { myAnimator.Play("Base Layer.birbIdle003", 0, 0); }
+        if (randomAnimState == 4) { myAnimator.Play("Base Layer.birbIdle004", 0, 0); }
+        if (randomAnimState == 5) { myAnimator.Play("Base Layer.birbIdle005", 0, 0); }
 
-        
+
+
+
 
 
     }
 
     void Update()
     {
-      
-     
-       
+
+        //set overall dist
+        ooverallDist = Vector3.Distance(player.transform.position, this.transform.position);
+
+
         //idk if vector distance is a good
         // I heard from a guy...that is not good for performance.
 
-           if(player.transform.position.z<this.transform.position.z+2 && player.transform.position.z>this.transform.position.z-2)
-       {
-           // myAnimator.Play("Base Layer.birb001flysoutht",0,0);
-          // animationStarted=true;   
-          zDistance=true;
-
-       }else{ zDistance=false;}
-
-
-        if (Vector3.Distance(player.position, transform.position) < range && animationStarted==false && zDistance==true) 
+        if (player.transform.position.z < this.transform.position.z + 2 && player.transform.position.z > this.transform.position.z - 2)
         {
-         
-      
-     
-       if(player.transform.position.x>this.transform.position.x+2 && animationStarted==false)
-       {
-         myAnimator.Play("Base Layer.birb001flyLeft",0,0);
-           animationStarted=true;
-           randomAnimState=-10;   
+            // myAnimator.Play("Base Layer.birb001flysoutht",0,0);
+            // animationStarted=true;   
+            zDistance = true;
 
-       }
+        }
+        else { zDistance = false; }
 
-           if(player.transform.position.x<this.transform.position.x+2 && animationStarted==false)
-       {
-         myAnimator.Play("Base Layer.birb001flyRight",0,0);
-           animationStarted=true;   
-           randomAnimState=-10;   
 
-       }
+        if (Vector3.Distance(player.position, transform.position) < range && animationStarted == false && zDistance == true)
+        {
+
+
+
+            if (player.transform.position.x > this.transform.position.x + 2 && animationStarted == false)
+            {
+                myAnimator.Play("Base Layer.birb001flyLeft", 0, 0);
+                animationStarted = true;
+                randomAnimState = -10;
+
+            }
+
+            if (player.transform.position.x < this.transform.position.x + 2 && animationStarted == false)
+            {
+                myAnimator.Play("Base Layer.birb001flyRight", 0, 0);
+                animationStarted = true;
+                randomAnimState = -10;
+
+            }
 
         }
 
 
-//for Z?
+        //for Z?
 
-   if (Vector3.Distance(player.position, transform.position) < range && animationStarted==false && zDistance==false) 
+        if (Vector3.Distance(player.position, transform.position) < range && animationStarted == false && zDistance == false)
         {
-         
-      
-     
-       if(player.transform.position.z>this.transform.position.z+1 && animationStarted==false)
-       {
-         myAnimator.Play("Base Layer.birb001flysoutht",0,0);
-           animationStarted=true;   
-           randomAnimState=-10;   
 
-       }
 
-           if(player.transform.position.z<this.transform.position.z+1 && animationStarted==false)
-       {
-         myAnimator.Play("Base Layer.birb001flynortht",0,0);
-           animationStarted=true;  
-           randomAnimState=-10;    
 
-       }
+            if (player.transform.position.z > this.transform.position.z + 1 && animationStarted == false)
+            {
+                myAnimator.Play("Base Layer.birb001flysoutht", 0, 0);
+                animationStarted = true;
+                randomAnimState = -10;
+
+            }
+
+            if (player.transform.position.z < this.transform.position.z + 1 && animationStarted == false)
+            {
+                myAnimator.Play("Base Layer.birb001flynortht", 0, 0);
+                animationStarted = true;
+                randomAnimState = -10;
+
+            }
 
         }
+
+
+        if (ooverallDist > respawDist && animationStarted ==true) 
+        {
+            // Debug.Log("BIIRRDDD"); 
+            myAnimator.Play("Base Layer.landFromNorth", 0, 0);
+            animationStarted = false;
+          
+
+
+
+        }
+
+        if (transform.localPosition==Vector3.zero && randomAnimState==-10 && animationStarted == false)
+        {
+            randomAnimState = Random.Range(0, 5);
+             Debug.Log("BIIRRDDD");
+            //setting up RandomIddle
+            if (randomAnimState == 0) { myAnimator.Play("Base Layer.birbIdle000", 0, 0); }
+            if (randomAnimState == 1) { myAnimator.Play("Base Layer.birbIdle001", 0, 0); }
+            if (randomAnimState == 2) { myAnimator.Play("Base Layer.birbIdle002", 0, 0); }
+            if (randomAnimState == 3) { myAnimator.Play("Base Layer.birbIdle003", 0, 0); }
+            if (randomAnimState == 4) { myAnimator.Play("Base Layer.birbIdle004", 0, 0); }
+            if (randomAnimState == 5) { myAnimator.Play("Base Layer.birbIdle005", 0, 0); }
+
+
+        }
+
+
+
+
+
+
+
 
 
 
