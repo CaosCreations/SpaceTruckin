6e983084ce67637834105fbc25ae766255564aeb
Max <61809127+maxwellpark@users.noreply.github.com>
2024-02-16 10:09:42 +0000

    Dialogue editor tooling

--- Assets/Editor/DialogueEditor/DialogueEditor.cs
+++ Assets/Editor/DialogueEditor/DialogueEditor.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
@@ -18,5 +19,29 @@ public static class DialogueEditor
         }
         var info = DialogueDatabaseManager.GetSeenInfo();
         info.ForEach(i => Debug.Log(i));
+    }
+
+    [MenuItem("Space Truckin/Dialogue/Lori Day 1 Pre-Terminal")]
+    public static void LoriDay1PreTerminal()
+    {
+        if (DialogueDatabaseManager.Instance == null)
+        {
+            return;
+        }
+
+        var variables = new Dictionary<string, bool>
+        {
+            { "Lori_Convo_1_seen", true },
+            { "Lori_Convo_2_seen" , true },
+            { "Lori_Convo_3_seen", true  },
+            { "Lori_Convo_4_seen", true  },
+            { "Qza_Convo_1_seen", true  },
+            { "Pirates_Convo_1_seen", true },
+            { "PR_Convo_1_seen", true },
+            { "ULSS_Convo_1_Rajni_Third_seen", true },
+        };
+
+        DialogueDatabaseManager.Instance.UpdateDatabaseVariables(variables);
+        PlayerPrefsManager.SetCanvasTutorialPrefValue(UICanvasType.Terminal, CalendarManager.CurrentDate, false);
     }
 }
