402124ed7038c51d6aef632f526764734c055f5c
Max <61809127+maxwellpark@users.noreply.github.com>
2024-03-04 19:03:44 +0000

    Fix animation context by date lookup and animator refs (#1130)

--- Assets/Scripts/NPC/NPC.cs
+++ Assets/Scripts/NPC/NPC.cs
@@ -4,22 +4,34 @@ public class NPC : MonoBehaviour
 {
     [SerializeField]
     private NPCData npcData;
-
     public NPCData Data => npcData;
 
-    [SerializeField]
-    private NPCAnimated npcAnimated;
-
-    public NPCAnimated Animated => npcAnimated;
+    public Animator Animator { get; private set; }
 
     private void Awake()
     {
+        Animator = GetComponent<Animator>();
+
+        if (Animator == null)
+        {
+            Debug.LogWarning($"{gameObject.name} NPC has no {nameof(Animator)} reference assigned");
+        }
+
         if (Data == null)
         {
             //Debug.LogWarning($"{gameObject.name} NPC has no {nameof(NPCData)} ScriptableObject reference assigned");
             return;
         }
-        Data.LocationByDateContainer.InitLookup();
+
+        if (!Data.LocationByDateContainer.LocationsByDate.IsNullOrEmpty())
+        {
+            Data.LocationByDateContainer.InitLookup();
+        }
+
+        if (!Data.AnimationContextByDateContainer.AnimationContextsByDate.IsNullOrEmpty())
+        {
+            Data.AnimationContextByDateContainer.InitLookup();
+        }
     }
 
     public override string ToString()
