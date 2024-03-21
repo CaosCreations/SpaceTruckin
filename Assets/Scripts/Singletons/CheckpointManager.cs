49f0f8722f2b6bc7cc2a45e4b0649886d69d089b
Max <61809127+maxwellpark@users.noreply.github.com>
2024-03-21 21:08:31 +0000

    Discover the Ultimate Game-Changing Update: Valiant Mind-Blowing Fix for Vukra Convos and Hangar Tutorial Texts - Day 2 Checkpoint has Never Been More Comprehensive! (#1147)
    * Fuel cost
    
    * Repairs, not diagnostics
    
    * Auto complete convos for day 2 checkpoint
    
    * Destroy day 1 cutscene triggers when skipping to day 2
    
    * Fix variable names in conditions
    
    * Accept all 4 faction missions on day 2 skip

--- Assets/Scripts/Singletons/CheckpointManager.cs
+++ Assets/Scripts/Singletons/CheckpointManager.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using UnityEngine;
 
 public class CheckpointManager : MonoBehaviour
@@ -17,18 +18,28 @@ public class CheckpointManager : MonoBehaviour
 
     public static void Day2()
     {
-        // TODO: Load station scene if not already
-
         MissionUtils.StartMissionByName("Alcohol for adults", "B. Lrorllyl");
-        // TODO: May have to complete the convos corresponding to these jobs?
         MissionUtils.AcceptMissionByName("Vukra Aid");
         MissionUtils.AcceptMissionByName("Towels");
+        MissionUtils.AcceptMissionByName("Replacement Shipment");
+        MissionUtils.AcceptMissionByName("ULSS Samples");
 
-        var convoIds = new int[] { 166, 167, 171, 172, 163, 168, 205, 175, 51, 58, 45, };
+        var convoIds = new int[] { 45, 51, 58, 73, 74, 75, 76, 77, 78, 79, 80, 163, 166, 167, 168, 171, 172, 175, 205 };
         DialogueDatabaseManager.Instance.SetConversationsSeen(convoIds);
         PlayerPrefsManager.SetCanvasTutorialPrefValue(UICanvasType.Terminal, new Date(1, 1, 1), true);
         CalendarManager.SetDate(new Date(1, 1, 1));
         UIManager.LiftAccessSettings();
+
+        // Destroy triggers that are for convos that we've auto-completed
+        var cutsceneTriggers = FindObjectsOfType<CutsceneCollisionTrigger>();
+        var triggerNames = new[] { "VestaBabyReceivingTrigger", "MeetVestaCleaningTrigger", "MeetPiratesTrigger", };
+        foreach (var trigger in cutsceneTriggers)
+        {
+            if (triggerNames.Contains(trigger.name))
+            {
+                Destroy(trigger.gameObject);
+            }
+        }
 
         if (PlayerManager.PlayerMovementAnimation.IsHoldingBaby)
         {
